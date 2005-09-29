7a085e2849629eabb6720b7c503737d7baa9376f
Nir Soffer <nirs@freeshell.org>
2005-09-29 00:07:04 +0000

    merge moin--main--1.3--patch-928
    Patches applied:
    
     * arch@arch.thinkmo.de--2003-archives/moin--main--1.3--patch-928
       Fix error handling on failure, improved error display
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-28
       integrate new cgitb error handling
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-29
       huge merge from main, fixing strange conflicts in error.py and request.py
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-30
       cgitb refactoring
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-31
       fixed cgitb import
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-32
       merge from main
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-33
       adding MOIN_DEBUG to server scripts
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-34
       refacrot cgitb for eaiser customisation, use one view class in error with new simpler design, better error handling in multiconfig.
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-35
       merge from main
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-36
       Print simple text exception when cgitb fail from any reason
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-37
       remove changes not related to error handling
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-38
       raise more helpful error when there is no wikis list in farmconfig.py
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-39
       refactor getConfig, remove duplicate code, more correct error handling, use either farmconfig or wikiconfig mtime
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-40
       more clear error message when data or underlay missing or have wrong privilages
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-41
       update cgitb docstring
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-42
       make cgitb easiter to customize, simplify error.ErrorView, print cgitb traceback after the original traceback.
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-43
       refactoring multiconfig, make return values more clear, improving docstrings
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-44
       use only preformatted text in printException
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-45
       cgitb: improve code readability
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-46
       merge from main
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-47
       simplify the simple text traceback output
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-48
       moved failure handling to new failure module (save imports for cgi)
    
     * nirs@freeshell.org--2005/moin--fix--1.3--patch-49
       spelling, docstring cleanup
    
    
    imported from: moin--main--1.5--patch-50

--- MoinMoin/error.py
+++ MoinMoin/error.py
@@ -1,19 +1,20 @@
 # -*- coding: iso-8859-1 -*-
+""" MoinMoin errors
+
+Supply Error class and sub classes used to raise various errors
+
+@copyright: 2004, 2005 by Nir Soffer <nirs@freeshell.org>
+@license: GNU GPL, see COPYING for details.
 """
-    MoinMoin - supply common error classes.
-    
-    TODO: translate strings?
+import sys
+from MoinMoin import config
 
-    @copyright: 2004 by Nir Soffer
-    @license: GNU GPL, see COPYING for details.
-"""
-
-import sys, os
-
-from MoinMoin import config, version
 
 class Error(Exception):
     """ Base class for moin moin errors
+
+    Use this class when you raise errors or create sub classes that
+    may be used to display non ASCII error message.
     
     Standard errors work safely only with strings using ascii or
     unicode. This class can be used safely with both strings using
@@ -25,6 +26,7 @@ class Error(Exception):
             
     When you want to render an error, use unicode() or str() as needed.
     
+    TODO: translate strings?
     """
 
     def __init__(self, message):
@@ -53,112 +55,54 @@ class Error(Exception):
         """ Make it possible to access attributes like a dict """
         return getattr(self, item)
     
-    
-class FatalError(Error):
-    """ Raise only when we must exit now 
-    
-    This error is handled at very high level and display a user friendly
-    message with the error message you supply.
-    
-    Using this class will hide the python traceback which is very useful
-    to developers. So use it only for known fatal errors, when we know
-    what is the error, but still can't continue.
+
+class CompositeError(Error):
+    ''' Base class for exceptions containing an exception
+
+    Do not use this class but its more specific sub classes.
+
+    Useful for hiding low level error inside high level user error,
+    while keeping the inner error information for debugging.
+
+    Example::
+
+        class InternalError(CompositeError):
+            """ Raise for internal errors """
+        
+        try:
+            # code that might fail...
+        except HairyLowLevelError:
+            raise InternalError("Sorry, internal error occurred")
+            
+    When showing a traceback, both InternalError traceback and
+    HairyLowLevelError traceback are available.
+    '''
+
+    def __init__(self, message):
+        """ Save system exception info before this exception is raised """
+        Error.__init__(self, message)
+        self.innerException = sys.exc_info()
+   
+    def exceptions(self):
+        """ Return a list of all inner exceptions """
+        all = [self.innerException]
+        while 1:
+            lastException = all[-1][1]
+            try:
+                all.append(lastException.innerException)
+            except AttributeError:
+                break
+        return all
+
+   
+class FatalError(CompositeError):
+    """ Base class for fatal error we can't handle
 
     Do not use this class but its more specific sub classes.
     """
-    name = 'MoinMoin Fatal Error'
-
-    # Links on project site that might help to resolve the problem
-    baseurl = 'http://moinmoin.wikiwikiweb.de/'
-    links = ['HelpOnInstalling', 'HelpOnConfiguration', 'MoinMoinBugs']
-    
-    def __init__(self, message):
-        """ Extend Error with environment details """
-        Error.__init__(self, message)
-        # Get environment details
-        self.system = '%s (%s)' % (sys.platform, os.name)
-        self.python = 'Python %s (%s)' % (sys.version.split()[0], sys.executable)
-        self.moin = 'MoinMoin release %s (revision %s)' % (
-            version.release, version.revision)
-        
-    def asHTML(self):
-        """ Render error as html
-
-        We may not have any style sheets at this stage, using our own
-        styles.
-
-        @rtype: unicode
-        @return: error formatted as html
-        """
-        # Make html paragraphs from text paragraphs separated by empty lines
-        message = [u'<p>%s</p>' % p for p in unicode(self).split(u'\n\n')]
-        message = '\n'.join(message)
-
-        # Make links list in the project site
-        links = [u'<li><a href="%s%s">%s</a></li>' % (self.baseurl, link, link)
-                 for link in self.links]
-        links = u'<ul class="links">\n%s\n</ul>\n' % '\n'.join(links)
-
-        html = [
-            u'''
-<html>
-    <head>
-        <title>%(name)s</title>
-        <style type="text/css">
-            h1 {font-size: 1.3em; margin: 0}
-            .message {border: 1px solid gray; background: #f7f7f7; margin:20px;}
-            .content {padding: 15px;}
-            .info {font-size: 0.85em; color: gray;}
-            ul.info {margin: 0; padding: 0;}
-            ul.links {margin: 5px 15px; padding: 0;}
-            ul.info li, ul.links li {display: inline; margin: 0 5px;}
-            hr {background: none; height: 0; border: none; border-top: 1px dotted gray;
-                margin: 0; padding: 0;}
-        </style>
-    </head>
-    <body>
-        <div class="message">
-            <div class="content">
-                <h1>%(name)s</h1>''' % self,
-            message,
-            u'''
-                <ul class="info">
-                    <li>%(system)s</li>
-                    <li>%(python)s</li>
-                    <li>%(moin)s</li>
-                </ul>
-            </div>
-            <hr>''' % self,
-            links,
-            u'''
-        </div>
-    </body>
-</html>
-''',]
-        html = '\n'.join(html)
-        return html
-
-    def asLog(self):
-        """ Render error for logs
-
-        For log we don't need all the 'nice' stuff we show for a
-        user. Just the plain error.
-
-        @rtype: string
-        @return: error formatted for log
-        """
-        return '%s: %s' % (self.name, str(self))
-
 
 class ConfigurationError(FatalError):
-    """ Raise when fatal misconfiguration is found
-    """
-    name = 'MoinMoin Configuration Error'
-        
+    """ Raise when fatal misconfiguration is found """
 
 class InternalError(FatalError):
-    """ Raise when internal fatal error is found
-    """
-    name = 'MoinMoin Internal Error'
-
-
+    """ Raise when internal fatal error is found """
--- wiki/server/mointwisted.py
+++ wiki/server/mointwisted.py
@@ -23,6 +23,9 @@ sys.path.insert(0, '/path/to/wikiconfig')
 # Path to the directory where farmconfig is located (if different).
 ## sys.path.insert(0, '/path/to/farmconfig')
 
+# Debug mode - show detailed error reports
+## import os
+## os.environ['MOIN_DEBUG'] = '1'
 
 from MoinMoin.server.twistedmoin import TwistedConfig, makeApp
 
