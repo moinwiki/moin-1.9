fbc356ea82bfeea330afdc1d420a1a15eeadab51
Nir Soffer <nirs@freeshell.org>
2005-10-21 00:33:09 +0000

    show gui editor links when it can be used, reformat common.js
    imported from: moin--main--1.5--patch-128

--- wiki/htdocs/common/js/common.js
+++ wiki/htdocs/common/js/common.js
@@ -1,42 +1,52 @@
 //
 // MoinMoin commonly used JavaScript functions
 //
-function check_gui_editor(){
- var sAgent = navigator.userAgent.toLowerCase()
- // Internet Explorer
- if ( sAgent.indexOf("msie") != -1 && sAgent.indexOf("mac") == -1 && sAgent.indexOf("opera") == -1 ){
-  var sBrowserVersion = navigator.appVersion.match(/MSIE (.\..)/)[1]
-  return ( sBrowserVersion >= 5.5 )
- }
- // Gecko
- else if ( navigator.product == "Gecko" && navigator.productSub >= 20030210 )
-  return true
- // Safari
- else if ( sAgent.indexOf("safari") != -1 )
-  return ( sAgent.match( /safari\/(\d+)/ )[1] >= 312 ) // Build must be at least 312 (1.3)
- else
-  return false
+
+function can_use_gui_editor() {
+    var sAgent = navigator.userAgent.toLowerCase();
+ 
+    if (sAgent.indexOf("msie") != -1 && sAgent.indexOf("mac") == -1 &&
+        sAgent.indexOf("opera") == -1 ) {
+        // Internet Explorer
+        var sBrowserVersion = navigator.appVersion.match(/MSIE (.\..)/)[1];
+        return ( sBrowserVersion >= 5.5 );
+    } else if (navigator.product == "Gecko" && 
+               navigator.productSub >= 20030210) {
+        // Gecko
+        return true;
+    } else if (sAgent.indexOf("safari") != -1 ) {
+        // Safari - build must be at least 312 (1.3)
+        return (sAgent.match( /safari\/(\d+)/ )[1] >= 312 );
+    } else {
+        // Unknown browser, assume gui editor is not compatible
+        return false;
+    }
 }
 
-function onload(){
- var enable_gui_editor = check_gui_editor()
- if ( enable_gui_editor){
-  guieditlink = document.getElementById("guieditlink")
-  if ( guieditlink ){
-   guieditlink.style.display = 'inline'
-  }
-  editlink = document.getElementById("editlink")
-  if ( editlink ){
-   href = editlink.href.replace('editor=textonly', 'editor=guipossible')
-   editlink.href = href
-  }
-  switch2gui = document.getElementById("switch2gui")
-  if ( switch2gui ){
-   // TODO by some js coder: make the (invisible) "GUI editor" button visible.
-  }
- }
+function update_ui() {
+    if (can_use_gui_editor()) {
+        guieditlink = document.getElementById("guieditlink");
+        if (guieditlink) {
+            guieditlink.style.display = 'inline';
+        }
+        editlink = document.getElementById("editlink")
+        if (editlink) {
+            href = editlink.href.replace('editor=textonly',
+                                         'editor=guipossible');
+            editlink.href = href;
+        }
+        switch2gui = document.getElementById('switch2gui')
+        if (switch2gui) {
+            switch2gui.style.display = 'inline';
+        }
+    }
+}
+
+function onload() {
+    update_ui();    
+    // countdown is available when editing
+    try {countdown()} catch (e) {}
 }
 
 // Initialize after loading the page
-window.onload = onload
-
+window.onload = onload;
