b6f2a9872b6a7432639c4d0d707a3962bda49018
Thomas Waldmann <tw@waldmann-edv.de>
2005-10-31 20:40:54 +0000

    add halloween logo
    imported from: moin--main--1.5--patch-180

--- wiki/config/moinmaster.py
+++ wiki/config/moinmaster.py
@@ -23,7 +23,10 @@ class Config(FarmConfig):
     # basic options (you normally need to change these)
     sitename = u'MoinMaster' # [Unicode]
     interwikiname = 'MoinMaster'
-    
+
+    # See also url_prefix setting!
+    logo_string = u'<img src="/wiki/common/moinhalloween.png" alt="MoinMoin Halloween Logo">'
+
     # name of entry page / front page [Unicode], choose one of those:
     
     # a) if most wiki content is in a single language
