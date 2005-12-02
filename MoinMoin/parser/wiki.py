106a0c3f1554aa457d5c91a9af1dc23551dbc78a
Thomas Waldmann <tw@waldmann-edv.de>
2005-12-02 22:48:57 +0000

    fixed and extended link icons
    imported from: moin--main--1.5--patch-277

--- MoinMoin/parser/wiki.py
+++ MoinMoin/parser/wiki.py
@@ -379,7 +379,7 @@ class Parser:
             name = ''.join(name.split('.')[:-1])
             return self.formatter.image(src=word, alt=name)
         else:
-            return (self.formatter.url(1, word, css='www') +
+            return (self.formatter.url(1, word, css=scheme) +
                     self.formatter.text(word) +
                     self.formatter.url(0))
 
@@ -423,12 +423,11 @@ class Parser:
             return self.attachment(words, pretty_url=1)
 
         if wikiutil.isPicture(words[1]) and re.match(self.url_rule, words[1]):
-            return (self.formatter.url(1, words[0], 'external', unescaped=1) +
+            return (self.formatter.url(1, words[0], css='external', unescaped=1) +
                     self.formatter.image(title=words[0], alt=words[0], src=words[1]) +
                     self.formatter.url(0))
         else:
-            return (self.formatter.url(1, words[0], 'external',
-                                       type='www', unescaped=1) +
+            return (self.formatter.url(1, words[0], css='www', unescaped=1) +
                     self.formatter.text(words[1]) +
                     self.formatter.url(0))
 
