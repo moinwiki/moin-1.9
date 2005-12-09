07b562abb0125bb7c2c759bde7fa76da6ab130b8
Alexander Schremmer <alex@alexanderweb.de.tla>
2005-12-09 20:12:10 +0000

    Added SSO support/auth module for PHP based applications.
    imported from: moin--main--1.5--patch-305

--- MoinMoin/auth.py
+++ MoinMoin/auth.py
@@ -39,6 +39,7 @@
     method that authentified the user.
     
     @copyright: (c) Bastian Blank, Florian Festi, Thomas Waldmann
+    @copyright: MoinMoin:AlexanderSchremmer
     @license: GNU GPL, see COPYING for details.
 """
 
@@ -78,14 +79,15 @@ def moin_cookie(request, **kw):
     return None, True
 
 
-"""
-   idea: maybe we should call back to the request object like:
-         username, password, authenticated, authtype = request.getUserPassAuth()
-	 WhoEver   geheim    false          basic      (twisted, doityourself pw check)
-	 WhoEver   None      true           basic/...  (apache)
-	 
-         thus, the server specific code would stay in request object implementation.
-"""
+#
+#   idea: maybe we should call back to the request object like:
+#         username, password, authenticated, authtype = request.getUserPassAuth()
+#	 WhoEver   geheim    false          basic      (twisted, doityourself pw check)
+#	 WhoEver   None      true           basic/...  (apache)
+#	 
+#        thus, the server specific code would stay in request object implementation.
+#
+#     THIS IS NOT A WIKI PAGE ;-)
 	 
 def http(request, **kw):
     """ authenticate via http basic/digest/ntlm auth """
@@ -217,4 +219,82 @@ def interwiki(request, **kw):
     else:
         pass
         # XXX redirect to homewiki
+
+
+class php_session:
+    """ Authentication module for PHP based frameworks
+        Authenticates via PHP session cookie. Currently supported systems:
+
+        * eGroupware 1.2 ("egw")
+         * You need to configure eGroupware in the "header setup" to use
+           "php sessions plus restore"
+
+        @copyright: 2005 by MoinMoin:AlexanderSchremmer
+            - Thanks to Spreadshirt
+    """
+
+    def __init__(self, apps=['egw'], s_path="/tmp", s_prefix="sess_"):
+        """ @param apps A list of the enabled applications. See above for
+            possible keys.
+            @param s_path The path where the PHP sessions are stored.
+            @param s_prefix The prefix of the session files.
+        """
+        
+        self.s_path = s_path
+        self.s_prefix = s_prefix
+        self.apps = apps
+
+    def __call__(self, request):
+        def handle_egroupware(session):
+            """ Extracts name, fullname and email from the session. """
+            username = session['egw_session']['session_lid'].split("@", 1)[0]
+            known_accounts = session['egw_info_cache']['accounts']['cache']['account_data']
+            
+            # if the next line breaks, then the cache was not filled with the current
+            # user information
+            user_info = [value for key, value in known_accounts.items()
+                         if value['account_lid'] == username][0]
+            name = user_info.get('fullname', '')
+            email = user_info.get('email', '')
+            
+            dec = lambda x: x and x.decode("iso-8859-1")
+            
+            return dec(username), dec(email), dec(name)
+        
+        import Cookie
+        import urllib
+        from MoinMoin.user import User
+    
+        from MoinMoin.util import sessionParser
+    
+        try:
+            cookie = Cookie.SimpleCookie(request.saved_cookie)
+        except Cookie.CookieError: # ignore invalid cookies
+            cookie = None
+        if cookie:
+            for cookiename in cookie.keys():
+                cookievalue = urllib.unquote(cookie[cookiename].value).decode('iso-8859-1')
+                session = sessionParser.loadSession(cookievalue, path=self.s_path, prefix=self.s_prefix)
+                if session:
+                    if "egw" in self.apps and session.get('egw_session', None):
+                        username, email, name = handle_egroupware(session)
+                        break
+            else:
+                return None, True
+            
+            user = User(request, name=username, auth_username=username)
+            
+            changed = False
+            if name != user.aliasname:
+                user.aliasname = name
+                changed = True
+            if email != user.email:
+                user.email = email
+                changed = True
+            
+            if user:
+                user.create_or_update(changed)
+            if user and user.valid:
+                return user, False # return user object and stop processing auth method list
+        return None, True # return None and continue with next method in auth list
 
