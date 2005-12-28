1d297da4695414ec1a8d861440c58e71b620663e
TW <tw@waldmann-edv.de>
2005-12-28 23:52:51 +0000

    wiki parser: fix table attribute markup parsing
    imported from: moin--main--1.5--patch-349

--- MoinMoin/parser/wiki.py
+++ MoinMoin/parser/wiki.py
@@ -672,6 +672,10 @@ class Parser:
                         'arg': arg, 'key': key}
                 else:
                     attrs['bgcolor'] = '"#%s"' % arg
+            elif key == '=':
+                arg = parser.get_token()
+                this_key = attrdef.split('=')[0]
+                attrs[this_key] = arg
             else:
                 msg = ""
             #print "key: %s\nattrs: %s" % (key, str(attrs))
