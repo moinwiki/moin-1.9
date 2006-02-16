2cc10a7465352feca176010a5d22277ca279ca30
Alexander Schremmer <alex@alexanderweb.de.tla>
2006-02-16 17:43:13 +0000

    Fixed PythonErrorEditingFrontPage.
    imported from: moin--main--1.5--patch-448

--- MoinMoin/formatter/pagelinks.py
+++ MoinMoin/formatter/pagelinks.py
@@ -20,40 +20,11 @@ class Formatter(FormatterBase):
     
     # All these must be overriden here because they raise
     # NotImplementedError!@#! or return html?! in the base class.
-    set_highlight_re = null
-    url = null
-    image = null
-    smiley = null
-    text = null
-    strong = null
-    emphasis = null
-    underline = null
-    highlight = null
-    sup = null
-    sub = null
-    strike = null
-    code = null
-    preformatted = null
-    small = null
-    big = null
-    code_area = null
-    code_line = null
-    code_token = null
-    linebreak = null
-    paragraph = null
-    rule = null
-    icon = null
-    number_list = null
-    bullet_list = null
-    listitem = null
-    definition_list = null
-    definition_term = null
-    definition_desc = null
-    heading = null
-    table = null
-    table_row = null
-    table_cell = null
-    attachment_link = null
-    attachment_image = null
-    attachment_drawing = null
+    set_highlight_re = rawHTML = url = image = smiley = text = null
+    strong = emphasis = underline =  highlight = sup = sub = strike = null
+    code = preformatted = small = big = code_area = code_line = null
+    code_token = linebreak = paragraph = rule = icon = null
+    number_list = bullet_list = listitem = definition_list = null
+    definition_term = definition_desc = heading = table = null
+    table_row = table_cell = attachment_link = attachment_image = attachment_drawing = null
 
