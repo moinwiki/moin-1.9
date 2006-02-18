af6ceca95683a47d756ff936e65a765af1123a19
TW <tw@waldmann-edv.de>
2006-02-18 16:58:21 +0000

    added ldap_login and smb_mount auth plugins + sample config
    imported from: moin--main--1.5--patch-454

--- MoinMoin/auth.py
+++ MoinMoin/auth.py
@@ -38,8 +38,8 @@
     It also gives a kw arg "auth_method" that tells the name of the auth
     method that authentified the user.
     
-    @copyright: (c) Bastian Blank, Florian Festi, Thomas Waldmann
-    @copyright: MoinMoin:AlexanderSchremmer
+    @copyright: 2005-2006 Bastian Blank, Florian Festi, Thomas Waldmann
+    @copyright: 2005-2006 MoinMoin:AlexanderSchremmer
     @license: GNU GPL, see COPYING for details.
 """
 
@@ -182,6 +182,141 @@ def sslclientcert(request, **kw):
         return u, False
     else:
         return None, True
+
+
+def smb_mount(request, **kw):
+    """ mount a SMB server's share for username (using username/password for
+        authentication at the SMB server). This can be used if you need access
+        to files on some share via the wiki, but needs more code to be useful.
+        If you don't need it, don't use it.
+    """
+    username = kw.get('name')
+    password = kw.get('password')
+    login = kw.get('login')
+    logout = kw.get('logout')
+    cfg = request.cfg
+    verbose = cfg.smb_verbose
+    if verbose: request.log("got name=%s login=%r logout=%r" % (username, login, logout))
+    
+    # we just intercept login to mount the smb share
+    if not login:
+        return None, True
+    
+    import os, pwd, subprocess
+    web_username = cfg.smb_dir_user
+    web_uid = pwd.getpwnam(web_username)[2] # XXX better just use current uid?
+    mountpoint = cfg.smb_mountpoint % username
+    mountcmd = u"sudo mount -t cifs -o user=%(user)s,domain=%(domain)s,uid=%(uid)d,dir_mode=%(dir_mode)s,file_mode=%(file_mode)s,iocharset=%(iocharset)s //%(server)s/%(share)s %(mountpoint)s >%(log)s 2>&1" % {
+        'user': username,
+        'uid': web_uid,
+        'domain': cfg.smb_domain,
+        'server': cfg.smb_server,
+        'share': cfg.smb_share,
+        'mountpoint': mountpoint,
+        'dir_mode': cfg.smb_dir_mode,
+        'file_mode': cfg.smb_file_mode,
+        'iocharset': cfg.smb_iocharset,
+        'log': cfg.smb_log,
+    }
+    try:
+        os.makedirs(mountpoint) # the dir containing the mountpoint must be writeable for us!
+    except OSError, err:
+        pass
+    env = os.environ.copy()
+    env['PASSWD'] = password.encode(cfg.smb_coding)
+    subprocess.call(mountcmd.encode(cfg.smb_coding), env=env, shell=True)
+    return None, True
+
+
+def ldap_login(request, **kw):
+    """ get authentication data from form, authenticate against LDAP (or Active Directory),
+        fetch some user infos from LDAP and create a user profile for that user that must
+        be used by subsequent auth plugins (like moin_cookie) as we never return a user
+        object from ldap_login.
+    """
+    username = kw.get('name')
+    password = kw.get('password')
+    login = kw.get('login')
+    logout = kw.get('logout')
+    
+    cfg = request.cfg
+    verbose = cfg.ldap_verbose
+    
+    if verbose: request.log("got name=%s login=%r logout=%r" % (username, login, logout))
+    
+    # we just intercept login and logout for ldap, other requests have to be
+    # handled by another auth handler, thus we return None, True.
+    if not login and not logout:
+        return None, True
+    
+    import sys, re
+    import ldap
+    import traceback
+
+    u = None
+    coding = cfg.ldap_coding
+    try:
+        if verbose: request.log("LDAP: Trying to initialize %s." % cfg.ldap_uri)
+        l = ldap.initialize(cfg.ldap_uri)
+        if verbose: request.log("LDAP: Connected to LDAP server %s." % cfg.ldap_uri)
+        # you can use %(username)s and %(password)s here to get the stuff entered in the form:
+        ldap_binddn = cfg.ldap_binddn % locals()
+        ldap_bindpw = cfg.ldap_bindpw % locals()
+        l.simple_bind_s(ldap_binddn.encode(coding), ldap_bindpw.encode(coding))
+        if verbose: request.log("LDAP: Bound with binddn %s" % ldap_binddn)
+
+        filterstr = "(%s=%s)" % (cfg.ldap_name_attribute, username)
+        if verbose: request.log("LDAP: Searching %s" % filterstr)
+        lusers = l.search_st(cfg.ldap_base, cfg.ldap_scope,
+                             filterstr.encode(coding), timeout=cfg.ldap_timeout)
+        result_length = len(lusers)
+        if result_length != 1:
+            if result_length > 1:
+                request.log("LDAP: Search found more than one (%d) matches for %s." % (len(lusers), filterstr))
+            if result_length == 0:
+                if verbose: request.log("LDAP: Search found no matches for %s." % (filterstr, ))
+            return None, True
+
+        dn, ldap_dict = lusers[0]
+        if verbose:
+            request.log("LDAP: debug lusers = %r" % lusers)
+            for key,val in ldap_dict.items():
+                request.log("LDAP: %s: %s" % (key, val))
+
+        try:
+            if verbose: request.log("LDAP: DN found is %s, trying to bind with pw" % dn)
+            l.simple_bind_s(dn, password.encode(coding))
+            if verbose: request.log("LDAP: Bound with dn %s (username: %s)" % (dn, username))
+            
+            email = ldap_dict.get(cfg.ldap_email_attribute, [''])[0]
+            email = email.decode(coding)
+            sn, gn = ldap_dict.get('sn', [''])[0], ldap_dict.get('givenName', [''])[0]
+            aliasname = ''
+            if sn and gn:
+                aliasname = "%s, %s" % (sn, gn)
+            elif sn:
+                aliasname = sn
+            aliasname = aliasname.decode(coding)
+            
+            u = user.User(request, auth_username=username, password=password, auth_method='ldap', auth_attribs=('name', 'password', 'email', 'mailto_author',))
+            u.name = username
+            u.aliasname = aliasname
+            u.email = email
+            u.remember_me = 0 # 0 enforces cookie_lifetime config param
+            if verbose: request.log("LDAP: creating userprefs with name %s email %s alias %s" % (username, email, aliasname))
+            
+        except ldap.INVALID_CREDENTIALS, err:
+            request.log("LDAP: invalid credentials (wrong password?) for dn %s (username: %s)" % (dn, username))
+
+    except:
+        info = sys.exc_info()
+        request.log("LDAP: caught an exception, traceback follows...")
+        request.log(''.join(traceback.format_exception(*info)))
+
+    if u:
+        u.create_or_update(True)
+    return None, True # moin_cookie has to set the cookie and return the user obj
+
 
 def interwiki(request, **kw):
     # TODO use auth_method and auth_attribs for User object
