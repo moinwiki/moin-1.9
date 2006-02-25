c80a88cf6ce2194a133f415358da06d6270df62e
TW <tw@waldmann-edv.de>
2006-02-25 08:11:57 +0000

    changed filters to use mimetypes
    imported from: moin--main--1.5--patch-458

--- MoinMoin/filter/text.py
+++ MoinMoin/filter/text.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-    MoinMoin - plain text file Filter
+    MoinMoin - text/* file Filter
 
     We try to support more than ASCII here.
 
@@ -11,7 +11,7 @@
 import codecs
 
 def execute(indexobj, filename):
-    for enc in ('utf-8', 'iso-8859-15', 'latin-1', ):
+    for enc in ('utf-8', 'iso-8859-15', 'iso-8859-1', ):
         try:
             f = codecs.open(filename, "r", enc)
             data = f.read()
