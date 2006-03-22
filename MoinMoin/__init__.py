d28e86ed4e58d30588ca7b226cb34d0545c7743d
Thomas Waldmann <tw@waldmann-edv.de>
2006-03-22 11:41:52 +0000

    make some action menu entries translatable
    imported from: moin--main--1.5--patch-497

--- MoinMoin/__init__.py
+++ MoinMoin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-MoinMoin Version 1.5.2 patch-495
+MoinMoin Version 1.5.2 patch-496
 
 @copyright: 2000-2006 by Jürgen Hermann <jh@web.de>
 @license: GNU GPL, see COPYING for details.
