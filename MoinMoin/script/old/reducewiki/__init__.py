d88f902dbd87cfa3b9a49f4602f5b58838b7b0ae
Thomas Waldmann <tw@waldmann-edv.de>
2006-03-26 13:30:19 +0000

    moin maint reducewiki
    imported from: moin--main--1.5--patch-508

--- MoinMoin/script/old/reducewiki/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: iso-8859-1 -*-
-"""
-    MoinMoin - reduce a wiki to the latest revision of each page
-
-    @copyright: 2005 by Thomas Waldmann (MoinMoin:ThomasWaldmann)
-    @license: GNU GPL, see COPYING for details.
-"""
