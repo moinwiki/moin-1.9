58a663b92ef9c3b1adb4588047070218456d6fa0
Thomas Waldmann <tw@waldmann-edv.de>
2006-04-08 18:53:09 +0000

    update underlay, fix typo
    imported from: moin--main--1.5--patch-539

--- MoinMoin/__init__.py
+++ MoinMoin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-MoinMoin Version 1.5.2 patch-510
+MoinMoin Version 1.5.3-rc1 patch-538
 
 @copyright: 2000-2006 by Jürgen Hermann <jh@web.de>
 @license: GNU GPL, see COPYING for details.
