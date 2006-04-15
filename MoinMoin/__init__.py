6cc33846c5a5c66d6de0c8e5e534e38a89c1f936
TW <tw@waldmann-edv.de>
2006-04-15 19:01:34 +0000

    bumped version to 1.5.3 release, updated underlay
    imported from: moin--main--1.5--patch-575

--- MoinMoin/__init__.py
+++ MoinMoin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-MoinMoin Version 1.5.3-rc2 patch-539
+MoinMoin Version 1.5.3 patch-574
 
 @copyright: 2000-2006 by Jürgen Hermann <jh@web.de>
 @license: GNU GPL, see COPYING for details.
