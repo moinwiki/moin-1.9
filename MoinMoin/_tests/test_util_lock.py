2d7099f666fcb5ca45489b9e6130f86d4a7dc1dc
TW <tw@waldmann-edv.de>
2006-04-30 16:38:35 +0200

    add test for time.time() vs. os.stat().st_mtime consistency

--- MoinMoin/_tests/test_util_lock.py
+++ MoinMoin/_tests/test_util_lock.py
@@ -17,10 +17,29 @@ class ExclusiveLockTests(unittest.TestCase):
     
     def setUp(self):
         self.test_dir = tempfile.mkdtemp('', 'lock_')
+        self.test_dir_mtime_goal = time.time()
+        self.test_dir_mtime_reported = os.stat(self.test_dir).st_mtime
         self.lock_dir = os.path.join(self.test_dir, "lock")
         
     def tearDown(self):
         shutil.rmtree(self.test_dir)
+
+    def testBrokenTimeAPI(self):
+        """ util.lock: os.stat().mtime consistency with time.time()
+        
+            the timestamp os.stat reports as st_mtime on a fresh file should
+            be the same (or at least almost the same) as the time time.time()
+            reported at this time.
+            Differences of n*3600s are usually operating system bugs / limitations,
+            Win32 (as of Win XP SP2 + hotfixes 2006-04-30) is broken if you set
+            TZ to a different value than the rest of the system uses.
+            E.g. if you set "TZ=GMT1EDT" (and the rest of the system is setup
+            on german/berlin timezone), it will report 7200s difference in the
+            summer.
+        """
+        diff = self.test_dir_mtime_reported - self.test_dir_mtime_goal # diff should be 0 or near 0
+        self.failUnless(abs(diff) <= 2,
+                        "Operating System time.time / os.stat inconsistent (diff==%.1fs). Locking will NOT work correctly." % float(diff))
 
     def testTimeout(self):
         """ util.lock: ExclusiveLock: raise ValueError for timeout < 2.0 """
