e6f2490809eb1d588d1de44a8e4bbd6ce1568305
TW <tw@waldmann-edv.de>
2006-05-31 12:04:43 +0200

    allow '-' in usernames (backport to 1.5)

--- MoinMoin/__init__.py
+++ MoinMoin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-MoinMoin Version 1.6.0alpha 8e8130a1f46b+ tip
+MoinMoin Version 1.6.0alpha 43b158d3cf22+ tip
 
 @copyright: 2000-2006 by Jürgen Hermann <jh@web.de>
 @license: GNU GPL, see COPYING for details.
