f410a8b6bb358d09195822d9a26461c45742ea67
Franz Pletz <fpletz AT franz-pletz DOT org@localhost>
2006-06-19 11:16:04 +0200

    matching stemmed words works reliably and, most importantly, fast ;)

--- MoinMoin/search.py
+++ MoinMoin/search.py
@@ -90,15 +90,7 @@ class BaseExpression:
                 self.pattern = pattern
         else:
             pattern = re.escape(pattern)
-            if stemmed:
-                # XXX: works, but pretty CPU-intensive (obviously...)
-                self.search_re = re.compile(r'(?=^|[\s]+|[^%s]+)%s[%s]*' %
-                        (config.chars_lower, case and pattern or
-                            ''.join(['[%s%s]' % (ch.upper(), ch.lower())
-                                for ch in pattern]),
-                         config.chars_lower), re.U)
-            else:
-                self.search_re = re.compile(pattern, flags)
+            self.search_re = re.compile(pattern, flags)
             self.pattern = pattern
 
 
@@ -280,7 +272,23 @@ class TextSearch(BaseExpression):
         # Search in page body
         body = page.get_raw_body()
         for match in self.search_re.finditer(body):
-            matches.append(TextMatch(re_match=match))
+            if Xapian.use_stemming:
+                # somewhere in regular word
+                if body[match.start()] not in config.chars_upper and \
+                        body[match.start()-1] in config.chars_lower:
+                    continue
+
+                post = 0
+                for c in body[match.end():]:
+                    if c in config.chars_lower:
+                        post += 1
+                    else:
+                        break
+
+                matches.append(TextMatch(start=match.start(),
+                        end=match.end()+post))
+            else:
+                matches.append(TextMatch(re_match=match))
 
         # Decide what to do with the results.
         if ((self.negated and matches) or
@@ -368,7 +376,23 @@ class TitleSearch(BaseExpression):
         # Get matches in page name
         matches = []
         for match in self.search_re.finditer(page.page_name):
-            matches.append(TitleMatch(re_match=match))
+            if Xapian.use_stemming:
+                # somewhere in regular word
+                if page.page_name[match.start()] not in config.chars_upper and \
+                        page.page_name[match.start()-1] in config.chars_lower:
+                    continue
+
+                post = 0
+                for c in page.page_name[match.end():]:
+                    if c in config.chars_lower:
+                        post += 1
+                    else:
+                        break
+
+                matches.append(TitleMatch(start=match.start(),
+                        end=match.end()+post))
+            else:
+                matches.append(TitleMatch(re_match=match))
         
         if ((self.negated and matches) or
             (not self.negated and not matches)):
