12b852c4739078c273e194be5132b06d699dfdd4
Alexander Schremmer <alex AT alexanderweb DOT de@localhost>
2006-07-01 01:52:41 +0200

    Replaced the module doc string.

--- MoinMoin/util/bdiff.py
+++ MoinMoin/util/bdiff.py
@@ -1,12 +1,13 @@
-# Binary patching and diffing
-#
-# Copyright 2005 Matt Mackall <mpm@selenic.com>
-# Copyright 2006 MoinMoin:AlexanderSchremmer
-#
-# Algorithm taken from mercurial's mdiff.py
-#
-# This software may be used and distributed according to the terms
-# of the GNU General Public License, incorporated herein by reference.
+"""
+    MoinMoin - Binary patching and diffing
+
+    @copyright: 2005 Matt Mackall <mpm@selenic.com>
+    @copyright: 2006 MoinMoin:AlexanderSchremmer
+    
+    Algorithm taken from mercurial's mdiff.py
+    
+    @license: GNU GPL, see COPYING for details.
+"""
 
 import zlib, difflib, struct
 
