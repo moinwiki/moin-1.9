7bc33dee27ed8441865e0e5eabaf5b550a4519ad
TW <tw@waldmann-edv.de>
2006-07-16 13:51:22 +0200

    whitespace-only cleanup in request/*

--- MoinMoin/request/WSGI.py
+++ MoinMoin/request/WSGI.py
@@ -17,42 +17,42 @@ class Request(RequestBase):
         try:
             self.env = env
             self.hasContentType = False
-            
+
             self.stdin = env['wsgi.input']
             self.stdout = StringIO.StringIO()
-            
+
             self.status = '200 OK'
             self.headers = []
-            
+
             self._setup_vars_from_std_env(env)
             RequestBase.__init__(self, {})
 
         except Exception, err:
             self.fail(err)
-    
+
     def setup_args(self, form=None):
         # TODO: does this include query_string args for POST requests?
         # see also how CGI works now
         if form is None:
             form = cgi.FieldStorage(fp=self.stdin, environ=self.env, keep_blank_values=1)
         return self._setup_args_from_cgi_form(form)
-    
+
     def read(self, n=None):
         if n is None:
             return self.stdin.read()
         else:
             return self.stdin.read(n)
-    
+
     def write(self, *data):
         self.stdout.write(self.encode(data))
-    
+
     def reset_output(self):
         self.stdout = StringIO.StringIO()
-    
+
     def setHttpHeader(self, header):
         if type(header) is unicode:
             header = header.encode('ascii')
-        
+
         key, value = header.split(':', 1)
         value = value.lstrip()
         if key.lower() == 'content-type':
@@ -62,27 +62,27 @@ class Request(RequestBase):
                 return
             else:
                 self.hasContentType = True
-        
+
         elif key.lower() == 'status':
             # save status for finish
             self.status = value
             return
-            
+
         self.headers.append((key, value))
-    
+
     def http_headers(self, more_headers=[]):
         for header in more_headers:
             self.setHttpHeader(header)
-        
+
         if not self.hasContentType:
             self.headers.insert(0, ('Content-Type', 'text/html;charset=%s' % config.charset))
-    
+
     def flush(self):
         pass
-    
+
     def finish(self):
         pass
-    
+
     def output(self):
         return self.stdout.getvalue()
 
