1c96fd5d1ee4d5050bf65747e538730526194efa
TW <tw@waldmann-edv.de>
2006-07-30 22:11:38 +0200

    don't raise an exception in emit_http_headers if we are called from exception handler

---
+++ MoinMoin/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: iso-8859-1 -*-
+"""
+MoinMoin Version 1.6.0alpha 61142a50c41b+ tip
+
+@copyright: 2000-2006 by Jürgen Hermann <jh@web.de>
+@license: GNU GPL, see COPYING for details.
+"""
+
+
+
