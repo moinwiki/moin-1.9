118a3411b0ace7741633d25eb5dda63708395e47
TW <tw@waldmann-edv.de>
2006-08-18 21:20:15 +0200

    remove most chmod calls, use 1 os.umask() call in request module instead (port from 1.5)

---
+++ MoinMoin/macro/LikePages.py
@@ -0,0 +1,25 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - Create list of LikePages
+
+    @copyright: 2004 by Johannes Berg <johannes@sipsolutions.de>
+    @license: GNU GPL, see COPYING for details.
+"""
+
+Dependencies = ['namespace']
+
+from MoinMoin.action import LikePages
+
+def execute(macro, args):
+    request = macro.request
+    pagename = macro.formatter.page.page_name
+
+    # Get matches
+    start, end, matches = LikePages.findMatches(pagename, request)
+
+    # Render matches
+    if matches and not isinstance(matches, (str, unicode)):
+        return request.redirectedOutput(LikePages.showMatches, pagename, request, start, end, matches, False)
+
+    return args
+
