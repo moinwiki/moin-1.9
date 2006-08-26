29e0492a1c28ac09a23e403f1719458c5c00c08f
TW <tw@waldmann-edv.de>
2006-08-26 22:53:58 +0200

    move some js to common.js

---
+++ wiki/config/mailimportconf.py
@@ -0,0 +1,7 @@
+# This is the configuration file for the mail import client
+
+# This secret has to be known by the wiki server
+mailimport_secret = u"foo"
+
+# The target wiki URL 
+mailimport_url = u"http://localhost/?action=xmlrpc2"
