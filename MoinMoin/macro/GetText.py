8d560efc71283fe7effbda788a21102a72bb5f98
TW <tw@waldmann-edv.de>
2007-01-07 22:55:02 +0100

    just using WikiName: as abbreviation for WikiName: gives too many false positives

---
+++ MoinMoin/macro/GetText.py
@@ -0,0 +1,23 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - Load I18N Text
+
+    This macro has the main purpose of supporting Help* page authors
+    to insert the texts that a user actually sees on his screen into
+    the description of the related features (which otherwise could
+    get very confusing).
+
+    @copyright: 2001 by Jürgen Hermann <jh@web.de>
+    @license: GNU GPL, see COPYING for details.
+"""
+
+from MoinMoin import wikiutil
+
+Dependencies = ["language"]
+
+def execute(macro, args):
+    """ Return a translation of args, or args as is """
+    translation = macro.request.getText(args, formatted=False)
+
+    return macro.formatter.text(translation)
+
---
+++ tests/maketestwiki.py
@@ -0,0 +1,65 @@
+# -*- coding: iso-8859-1 -*-
+"""
+MoinMoin - make a test wiki
+
+Usage:
+
+    maketestwiki.py
+
+@copyright: 2005 by Thomas Waldmann
+@license: GNU GPL, see COPYING for details.
+"""
+
+import os, sys, shutil, errno, tarfile
+
+moinpath = os.path.join(os.path.dirname(sys.argv[0]), os.pardir)
+sys.path.insert(0, os.path.abspath(moinpath))
+
+WIKI = os.path.abspath(os.path.join('tests', 'wiki'))
+SHARE = os.path.abspath('wiki')
+
+def removeTestWiki():
+    print 'removing old wiki ...'
+    for dir in ['data', 'underlay']:
+        try:
+            shutil.rmtree(os.path.join(WIKI, dir))
+        except OSError, err:
+            if not (err.errno == errno.ENOENT or
+                    (err.errno == 3 and os.name == 'nt')):
+                raise
+
+def copyData():
+    print 'copying data ...'
+    src = os.path.join(SHARE, 'data')
+    dst = os.path.join(WIKI, 'data')
+    shutil.copytree(src, dst)
+    # Remove arch-ids dirs
+    for path, dirs, files in os.walk(dst):
+        for dir in dirs[:]:
+            if dir == '.arch-ids':
+                shutil.rmtree(os.path.join(path, dir))
+                dirs.remove(dir)
+
+
+def untarUnderlay():
+    print 'untaring underlay ...'
+    tar = tarfile.open(os.path.join(SHARE, 'underlay.tar.bz2'), mode='r:bz2')
+    for member in tar:
+        tar.extract(member, WIKI)
+    tar.close()
+
+
+def run():
+    try:
+        os.makedirs(WIKI)
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            raise
+
+    removeTestWiki()
+    copyData()
+    untarUnderlay()
+
+if __name__ == '__main__':
+    run()
+
