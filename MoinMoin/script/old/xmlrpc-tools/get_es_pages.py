b42535966e4dd1c3deb79ea032d1bd5044b39d79
ReimarBauer <rb.proj@gmail.com>
2007-01-26 19:45:07 +0100

    action macro: Load for uploading and page creation and Save for downloading a page added

---
+++ MoinMoin/script/old/xmlrpc-tools/get_es_pages.py
@@ -0,0 +1,32 @@
+""" get some pages from another wiki """
+
+def run():
+    import sys, os, xmlrpclib, codecs
+
+    sys.path.insert(0, "..")
+    from MoinMoin import wikiutil
+
+    s = xmlrpclib.ServerProxy("http://wainu.ii.uned.es/wainuki/?action=xmlrpc2")
+    index = open("index")
+
+    for l in index:
+        d = l.split('||')
+        pn = d[3].strip()
+        pd = s.getPage(pn)
+        dn = wikiutil.quoteWikinameFS(pn.decode("utf-8"))
+        os.mkdir(dn)
+        cn = os.path.join(dn,'current')
+        f = open(cn,'w')
+        f.write('00000001\n')
+        f.close()
+        dn2 = os.path.join(dn, 'revisions')
+        os.mkdir(dn2)
+        fn = os.path.join(dn2,'00000001')
+        f = codecs.open(fn,"wb","utf-8")
+        pd = pd.replace('\n','\r\n')
+        f.write(pd)
+        f.close()
+
+if __name__ == "__main__":
+    run()
+
