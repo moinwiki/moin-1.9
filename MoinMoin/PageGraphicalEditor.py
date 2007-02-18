aefe82ee8a1965ec7cb01e83fd52a838a6517478
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2007-02-18 22:22:14 +0100

    call send_title with page kw arg, if available, not with pagename

--- MoinMoin/PageGraphicalEditor.py
+++ MoinMoin/PageGraphicalEditor.py
@@ -204,7 +204,7 @@ Please review the page and save then. Do not save this page as it is!""")
         request.theme.send_title(
             title % {'pagename': self.split_title(), },
             page=self,
-            pagename=self.page_name, msg=status,
+            msg=status,
             html_head=self.lock.locktype and (
                 PageEditor._countdown_js % {
                      'countdown_script': request.theme.externalScript('countdown'),
