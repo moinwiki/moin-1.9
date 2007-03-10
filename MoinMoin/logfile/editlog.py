ed9abf88ecc787588c2f675710c7b6aa80b67b2e
TW <tw@waldmann-edv.de>
2007-03-10 20:54:09 +0100

    remove backticks, using repr() instead

--- MoinMoin/logfile/editlog.py
+++ MoinMoin/logfile/editlog.py
@@ -201,7 +201,7 @@ class EditLog(LogFile):
         expr = "1"
         for field in ['pagename', 'addr', 'hostname', 'userid']:
             if kw.has_key(field):
-                expr = "%s and x.%s == %s" % (expr, field, `kw[field]`)
+                expr = "%s and x.%s == %s" % (expr, field, repr(kw[field]))
 
         if kw.has_key('ed_time_usecs'):
             expr = "%s and long(x.ed_time_usecs) == %s" % (expr, long(kw['ed_time_usecs'])) # must be long for py 2.2.x
