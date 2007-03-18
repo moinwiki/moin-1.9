a697fede38d7d78955d0b34ce1c6ca7ac7e1674e
ReimarBauer <rb.proj@gmail.com>
2007-03-18 23:19:49 +0100

    SpellCheck: check if user may write to request.cfg.page_local_spelling_words

---
+++ MoinMoin/action/revert.py
@@ -0,0 +1,44 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - revert a page to a previous revision
+
+    @copyright: 2000-2004 by Jürgen Hermann <jh@web.de>,
+                2006 by MoinMoin:ThomasWaldmann
+    @license: GNU GPL, see COPYING for details.
+"""
+from MoinMoin.Page import Page
+
+def execute(pagename, request):
+    """ restore another revision of a page as a new current revision """
+    from MoinMoin.PageEditor import PageEditor
+    _ = request.getText
+    msg = None
+    rev = request.rev
+    pg = Page(request, pagename, rev=rev)
+
+    if not request.user.may.revert(pagename):
+        msg = _('You are not allowed to revert this page!')
+    elif rev is None:
+        msg = _('You were viewing the current revision of this page when you called the revert action. '
+                'If you want to revert to an older revision, first view that older revision and '
+                'then call revert to this (older) revision again.')
+    else:
+        newpg = PageEditor(request, pagename)
+
+        revstr = '%08d' % rev
+        try:
+            msg = newpg.saveText(pg.get_raw_body(), 0, extra=revstr, action="SAVE/REVERT")
+            pg = newpg
+        except newpg.SaveError, msg:
+            msg = unicode(msg)
+        request.reset()
+
+        key = request.form.get('key', ['text_html'])[0]
+
+        # Remove cache entry (if exists)
+        pg = Page(request, pagename)
+        from MoinMoin import caching
+        caching.CacheEntry(request, pg, key, scope='item').remove()
+        caching.CacheEntry(request, pg, "pagelinks", scope='item').remove()
+
+    pg.send_page(msg=msg)
---
+++ MoinMoin/filter/image_jpeg.py
@@ -0,0 +1,30 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - image/jpeg Filter
+
+    @copyright: 2006 by ThomasWaldmann MoinMoin:ThomasWaldmann
+    @license: GNU GPL, see COPYING for details.
+"""
+import EXIF
+
+def execute(indexobj, filename):
+    """ Extract some EXIF data """
+    try:
+        f = file(filename, 'rb')
+        tags = EXIF.process_file(f)
+        f.close()
+        # get rid of some big stuff:
+        try:
+            del tags["JPEGThumbnail"]
+        except:
+            pass
+        try:
+            del tags["EXIF MakerNote"]
+        except:
+            pass
+        data = str(tags).decode('utf-8')
+    except (ValueError, TypeError): # EXIF throws ValueError on unknown tags
+                                    #             TypeError on other occassions
+        data = u''
+    return data
+
