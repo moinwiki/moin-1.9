9f29a3166f0f8f2071bbb60df76b783f8bae1a87
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2007-04-01 15:18:39 +0200

    update the page trail after successfully saving a page, needed for NewPage macro because it does not send the newly created page

--- MoinMoin/PageEditor.py
+++ MoinMoin/PageEditor.py
@@ -1147,6 +1147,11 @@ Please review the page and save then. Do not save this page as it is!""")
                     else:
                         index.update_page(self.page_name)
 
+            # Update page trail with the page we just saved.
+            # This is needed for NewPage macro with backto because it does not
+            # send the page we just saved.
+            request.user.addTrail(self.page_name)
+
         # remove lock (forcibly if we were allowed to break it by the UI)
         # !!! this is a little fishy, since the lock owner might not notice
         # we broke his lock ==> but revision checking during preview will
