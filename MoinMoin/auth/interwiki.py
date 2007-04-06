58499c445902c6fb7a0ae7686a4c91791b1c66d4
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2007-04-06 21:36:26 +0200

    interwiki auth: fixes and docs

--- MoinMoin/auth/interwiki.py
+++ MoinMoin/auth/interwiki.py
@@ -6,6 +6,9 @@
                 2007 by MoinMoin:ThomasWaldmann
     @license: GNU GPL, see COPYING for details.
 """
+
+verbose = False
+
 import xmlrpclib
 from MoinMoin import auth, wikiutil, user
 
@@ -16,25 +19,30 @@ def interwiki(request, **kw):
     user_obj = kw.get('user_obj')
 
     if login:
-        wikitag, wikiurl, wikitail, err = wikiutil.resolve_wiki(username)
+        if verbose: request.log("interwiki auth: trying to auth %r" % username)
+        username = username.replace(' ', ':', 1) # Hack because ':' is not allowed in name field
+        wikitag, wikiurl, name, err = wikiutil.resolve_wiki(request, username)
 
+        if verbose: request.log("interwiki auth: resolve wiki returned: %r %r %r %r" % (wikitag, wikiurl, name, err))
         if err or wikitag not in request.cfg.trusted_wikis:
             return user_obj, True
 
         if password:
             homewiki = xmlrpclib.Server(wikiurl + "?action=xmlrpc2")
-            account_data = homewiki.getUser(wikitail, password)
+            account_data = homewiki.getUser(name, password)
             if isinstance(account_data, str):
                 # e.g. "Authentication failed", TODO: show error message
+                if verbose: request.log("interwiki auth: %r wiki said: %s" % (wikitag, account_data))
                 return user_obj, True
 
             # TODO: check auth_attribs items
-            u = user.User(request, name=username, auth_method='interwiki', auth_attribs=('name', 'aliasname', 'password', 'email', ))
+            u = user.User(request, name=name, auth_method='interwiki', auth_attribs=('name', 'aliasname', 'password', 'email', ))
             for key, value in account_data.iteritems():
                 if key not in request.cfg.user_transient_fields:
                     setattr(u, key, value)
-            if u:
-                u.create_or_update(True)
+            u.valid = 1
+            u.create_or_update(True)
+            if verbose: request.log("interwiki: successful auth for %r" % name)
             return u, True # moin_session has to set the cookie
 
     return user_obj, True
