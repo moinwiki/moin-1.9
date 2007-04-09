b083b7da8b0bda3f40471c5894c63be4ba4e18d3
ReimarBauer <rb.proj@gmail.com>
2007-04-09 07:36:59 +0000

    EmbedObject:minor optimisation

---
+++ MoinMoin/_tests/test_macro.py
@@ -0,0 +1,34 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - MoinMoin.macro Tests
+
+    @copyright: 2003-2004 by Juergen Hermann <jh@web.de>,
+                2006 MoinMoin:ThomasWaldmann
+    @license: GNU GPL, see COPYING for details.
+"""
+
+import unittest
+
+from MoinMoin import macro
+from MoinMoin.parser.text import Parser
+from MoinMoin.formatter.text_html import Formatter
+
+
+class MacroTestCase(unittest.TestCase):
+    def testTrivialMacro(self):
+        """macro: trivial macro works"""
+        m = self._make_macro()
+        expected = m.formatter.linebreak(0)
+        result = m.execute("BR", "")
+        self.assertEqual(result, expected,
+            'Expected "%(expected)s" but got "%(result)s"' % locals())
+
+    def _make_macro(self):
+        """Test helper"""
+        p = Parser("##\n", self.request)
+        p.formatter = Formatter(self.request)
+        self.request.formatter = p.formatter
+        p.form = self.request.form
+        m = macro.Macro(p)
+        return m
+
