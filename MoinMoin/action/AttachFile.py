c354c476e96b4fff1bb80be2656b8ec07938cd8c
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2007-04-20 21:59:19 +0200

    AttachFile: now really fixed extensions for packages

--- MoinMoin/action/AttachFile.py
+++ MoinMoin/action/AttachFile.py
@@ -342,8 +342,8 @@ def _build_filelist(request, pagename, showheader, readonly, mime_type='*'):
             else:
                 viewlink = '<a href="%(baseurl)s/%(urlpagename)s?action=%(action)s&amp;do=view&amp;target=%(urlfile)s">%(label_view)s</a>' % parmdict
 
-            if (packages.ZipPackage(request, os.path.join(attach_dir, file).encode(config.charset)).isPackage() and mt.minor == 'zip' and
-                request.user.isSuperUser()):
+            if (packages.ZipPackage(request, os.path.join(attach_dir, file).encode(config.charset)).isPackage() and
+                 request.user.isSuperUser()):
                 viewlink += ' | <a href="%(baseurl)s/%(urlpagename)s?action=%(action)s&amp;do=install&amp;target=%(urlfile)s">%(label_install)s</a>' % parmdict
             elif (zipfile.is_zipfile(os.path.join(attach_dir, file).encode(config.charset)) and
                 mt.minor == 'zip' and request.user.may.read(pagename) and request.user.may.delete(pagename)
