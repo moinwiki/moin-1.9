451db15ed7449d25cc1ea03d1986eb9c0ff4227a
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2007-04-26 12:55:30 +0200

    wikiutil.searchAndImportPlugin: type2classname, converter removed

--- MoinMoin/wikiutil.py
+++ MoinMoin/wikiutil.py
@@ -925,7 +925,7 @@ class MimeType(object):
         format = format.lower()
         if format in ('plain', 'csv', 'rst', 'docbook', 'latex', 'tex', 'html', 'css',
                       'xml', 'python', 'perl', 'php', 'ruby', 'javascript',
-                      'cplusplus', 'java', 'pascal', 'diff', 'gettext', 'text_html_text_moin_wiki', 'xslt',):
+                      'cplusplus', 'java', 'pascal', 'diff', 'gettext', 'xslt', ):
             mimetype = 'text', format
         else:
             mapping = {
@@ -1113,7 +1113,6 @@ def getPlugins(kind, cfg):
 def searchAndImportPlugin(cfg, type, name, what=None):
     type2classname = {"parser": "Parser",
                       "formatter": "Formatter",
-                      "converter": "convert",
     }
     if what is None:
         what = type2classname[type]
