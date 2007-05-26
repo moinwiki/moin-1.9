9a5278210f9de49108113b5e8ac5a8b0c9284066
TW <tw@waldmann-edv.de>
2007-05-26 22:57:57 +0200

    AdvancedSearch: added i18n for 'any mimetype'

--- MoinMoin/macro/AdvancedSearch.py
+++ MoinMoin/macro/AdvancedSearch.py
@@ -90,7 +90,7 @@ def advanced_ui(macro):
     # mimetype selection
     mimetype = form_get(request, 'mimetype')
     mt_select = makeSelection('mimetype',
-            [('', 'any mimetype')] + [(m[1], '*%s - %s' % m) for m in sorted(mimetypes.types_map.items())],
+            [('', _('any mimetype'))] + [(m[1], '*%s - %s' % m) for m in sorted(mimetypes.types_map.items())],
             mimetype)
 
 
