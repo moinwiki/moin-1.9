ec6fb295305a2f5e2b71d8a41d8bd0480fbff24c
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-05-31 01:03:54 +0200

    Add a stub for jabber notification plugin. It currently only prints info about received events.

---
+++ MoinMoin/events/JabberNotification.py
@@ -0,0 +1,14 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - jabber notification plugin from event system
+
+    This code sends notifications using a separate daemon.
+
+@copyright: 2007 by Karol Nowak <grywacz@gmail.com>
+@license: GNU GPL, see COPYING for details.
+"""
+
+# nothing here yet 8)
+
+def handle(event):
+    print event
