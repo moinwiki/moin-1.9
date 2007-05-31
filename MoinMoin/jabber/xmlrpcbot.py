fa7a6d58d950f5646e6429ca5f67a9b330986e63
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-05-31 21:23:16 +0200

    Handle inbound xmlrpc traffic, send notifications. This is only a beginning. ;)

--- MoinMoin/jabber/xmlrpcbot.py
+++ MoinMoin/jabber/xmlrpcbot.py
@@ -10,9 +10,11 @@
     @license: GNU GPL, see COPYING for details.
 """
 
+import time
 from threading import Thread
+from SimpleXMLRPCServer import SimpleXMLRPCServer 
 
-class Notification:
+class NotificationCommand:
     """Class representing a notification request"""
     
     def __init__(self, jid, text):
@@ -26,17 +28,41 @@ class XMLRPCClient(Thread):
     a wiki, as inctructed by command objects received from
     the XMPP component"""
     
-    def __init__(self, commands):
+    def __init__(self, config, commands):
         Thread.__init__(self)
         self.commands = commands
+        
+    def run(self):
+        pass
 
-class XMLRPCServer(Thread):
+class XMLRPCServer(Thread, SimpleXMLRPCServer):
     """XMLRPC Server
     
     It waits for notifications requests coming from wiki,
     creates command objects and puts them on a queue for
     later processing by the XMPP component"""
     
-    def __init__(self, commands):
+    def __init__(self, config, commands):
         Thread.__init__(self)
+        SimpleXMLRPCServer.__init__(self, (config.xmlrpc_host, config.xmlrpc_port) )
         self.commands = commands
+        self.verbose = config.verbose
+        
+    def run(self):
+        """Starts the server / thread"""
+        
+        self.register_function(self.send_notification)
+        self.serve_forever()
+        
+    def log(self, message):
+        """Logs a message and its timestamp"""
+        
+        t = time.localtime( time.time() )
+        print time.strftime("%H:%M:%S", t), message
+
+    def send_notification(self, jid, text):
+        """Instructs the XMPP component to send a notification"""
+        
+        n = NotificationCommand(jid, text)
+        self.commands.put_nowait(n)
+        return True
