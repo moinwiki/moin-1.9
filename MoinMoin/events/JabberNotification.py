407b91c51b466add4ee82cb214eb5d9242cf330b
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-05-31 21:21:38 +0200

    Actually send notifications. For now, only to me. :P

--- MoinMoin/events/JabberNotification.py
+++ MoinMoin/events/JabberNotification.py
@@ -8,7 +8,30 @@
 @license: GNU GPL, see COPYING for details.
 """
 
-# nothing here yet 8)
+import xmlrpclib
+from MoinMoin.events import PageEvent, PageChangedEvent
+
+server = None
 
 def handle(event):
-    print event
+    global server
+    
+    if not isinstance(event, PageEvent) or event.request.cfg.bot_host is None:
+        return
+    
+    # Create an XML RPC server object only if it doesn't exist
+    if server is None:
+        server = xmlrpclib.Server("http://" + event.request.cfg.bot_host)
+    
+    msg = u"Page %(page_name)s has been modified!\n" % ( {'page_name': event.page.page_name} )
+    
+    if event.comment:
+        msg = msg + u"The comment is: %(comment)s\n" % ( {'comment': event.comment} )
+    
+    if event.trivial:
+        msg = msg + u"This change has been marked as TRIVIAL.\n"
+        
+    try:
+        server.send_notification(u"grzyw@jabber.org", msg)
+    except Exception, desc:
+        print "XML RPC error:", desc
