e23892349b44106c7ec6d7740844536237b1ecdc
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2007-06-10 23:13:41 +0200

    epydoc target path for 1.7

--- MoinMoin/__init__.py
+++ MoinMoin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-MoinMoin Version 1.6.0alpha eb68960b1dfb+ tip
+MoinMoin Version 1.7.0alpha 
 
 @copyright: 2000-2006 by Juergen Hermann <jh@web.de>,
             2002-2007 MoinMoin:ThomasWaldmann
