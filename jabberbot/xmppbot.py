3f33f26fb553ad151dc9deb54a2d214e11bf4813
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-06-18 03:30:58 +0200

    Don't process commands with no jid and issue a warning. That's a proper fix.

--- jabberbot/xmppbot.py
+++ jabberbot/xmppbot.py
@@ -173,6 +173,11 @@ class XMPPBot(Client, Thread):
         @param ignore_dnd: if command results in user interaction, should DnD be ignored?
         
         """
+        
+        if not command.jid:
+            self.log("Received a command with empty jid, looks like a bug!")
+            return
+        
         # Handle normal notifications
         if isinstance(command, cmd.NotificationCommand):
             jid = JID(node_or_jid=command.jid)
