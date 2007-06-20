0c1bd821d587b2b4e96afe763405ef7c941c8fd4
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2007-06-20 20:27:46 +0200

    test of Hits moved to macro and converted for py.test, a test for the Action macro added

--- MoinMoin/_tests/macro/test_Hits.py
+++ MoinMoin/_tests/macro/test_Hits.py
@@ -6,23 +6,35 @@
 
     @license: GNU GPL, see COPYING for details.
 """
-import os, unittest # LEGACY UNITTEST, PLEASE DO NOT IMPORT unittest IN NEW TESTS, PLEASE CONSULT THE py.test DOCS
+import os
 from MoinMoin import macro
 from MoinMoin.logfile import eventlog
 from MoinMoin.Page import Page
 from MoinMoin.PageEditor import PageEditor
 from MoinMoin.parser.text_moin_wiki import Parser
 
-class TestHits(unittest.TestCase):
+class TestHits:
     """Hits: testing Hits macro """
 
-    pagename = u'AutoCreatedMoinMoinTemporaryTestPage'
-
-    def setUp(self):
+    def setup_class(self):
+        self.pagename = u'AutoCreatedMoinMoinTemporaryTestPageForHits'
         self.page = PageEditor(self.request, self.pagename)
+        self.shouldDeleteTestPage = False
+        # for that test eventlog needs to be empty
+        fpath = self.request.rootpage.getPagePath('event-log', isfile=1)
+        if os.path.exists(fpath):
+            os.remove(fpath)
 
-    def tearDown(self):
-        self.deleteTestPage()
+    def teardown_class(self):
+        if self.shouldDeleteTestPage:
+            import shutil
+            page = Page(self.request, self.pagename)
+            fpath = page.getPagePath(use_underlay=0, check_create=0)
+            shutil.rmtree(fpath, True)
+
+            fpath = self.request.rootpage.getPagePath('event-log', isfile=1)
+            if os.path.exists(fpath):
+                os.remove(fpath)
 
     def _make_macro(self):
         """Test helper"""
@@ -40,30 +52,16 @@ class TestHits(unittest.TestCase):
         m = self._make_macro()
         return m.execute(name, args)
 
-    def createTestPage(self, body):
+    def _createTestPage(self, body):
         """ Create temporary page """
         assert body is not None
         self.request.reset()
         self.page.saveText(body, 0)
 
-    def deleteTestPage(self):
-        """ Delete temporary page, bypass logs and notifications """
-        if self.shouldDeleteTestPage:
-            import shutil
-            shutil.rmtree(self.getPagePath(), True)
-
-            fpath = self.request.rootpage.getPagePath('event-log', isfile=1)
-            if os.path.exists(fpath):
-                os.remove(fpath)
-
-    def getPagePath(self):
-        page = Page(self.request, self.pagename)
-        return page.getPagePath(use_underlay=0, check_create=0)
-
     def testHitsNoArg(self):
         """ macro Hits test: 'no args for Hits (Hits is executed on current page) """
         self.shouldDeleteTestPage = False
-        self.createTestPage('This is an example to test a macro')
+        self._createTestPage('This is an example to test a macro')
 
         # Three log entries for the current page and one for WikiSandBox simulating viewing
         eventlog.EventLog(self.request).add(self.request, 'VIEWPAGE', {'pagename': 'WikiSandBox'})
@@ -73,13 +71,12 @@ class TestHits(unittest.TestCase):
 
         result = self._test_macro('Hits', '')
         expected = "3"
-        self.assertEqual(result, expected,
-                     '"%s" not in "%s"' % (expected, result))
+        assert result == expected
 
     def testHitsForAll(self):
         """ macro Hits test: 'all=1' for Hits (all pages are counted for VIEWPAGE) """
         self.shouldDeleteTestPage = False
-        self.createTestPage('This is an example to test a macro with parameters')
+        self._createTestPage('This is an example to test a macro with parameters')
 
         # Two log entries for simulating viewing
         eventlog.EventLog(self.request).add(self.request, 'VIEWPAGE', {'pagename': self.pagename})
@@ -87,8 +84,7 @@ class TestHits(unittest.TestCase):
 
         result = self._test_macro('Hits', 'all=1')
         expected = "6"
-        self.assertEqual(result, expected,
-                     '"%s" not in "%s"' % (expected, result))
+        assert result == expected
 
     def testHitsForFilter(self):
         """ macro Hits test: 'all=1, filter=SAVEPAGE' for Hits (SAVEPAGE counted for current page)"""
@@ -98,8 +94,7 @@ class TestHits(unittest.TestCase):
         eventlog.EventLog(self.request).add(self.request, 'SAVEPAGE', {'pagename': 'WikiSandBox'})
         result = self._test_macro('Hits', 'filter=SAVEPAGE')
         expected = "2"
-        self.assertEqual(result, expected,
-                     '"%s" not in "%s"' % (expected, result))
+        assert result == expected
 
     def testHitsForAllAndFilter(self):
         """ macro test: 'all=1, filter=SAVEPAGE' for Hits (all pages are counted for SAVEPAGE)"""
@@ -107,6 +102,6 @@ class TestHits(unittest.TestCase):
 
         result = self._test_macro('Hits', 'all=1, filter=SAVEPAGE')
         expected = "3"
-        self.assertEqual(result, expected,
-                     '"%s" not in "%s"' % (expected, result))
+        assert result == expected
+
 
