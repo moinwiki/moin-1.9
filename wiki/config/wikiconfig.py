66297aca0ed181da41d57441a0d546564e86b595
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2007-06-21 09:14:31 +0200

    wikiconfig: /wiki replaced by /moin_static170

--- wiki/config/wikiconfig.py
+++ wiki/config/wikiconfig.py
@@ -38,7 +38,7 @@ class Config(DefaultConfig):
     # Wiki logo. You can use an image, text or both. [Unicode]
     # For no logo or text, use '' - the default is to show the sitename.
     # See also url_prefix setting below!
-    logo_string = u'<img src="/wiki/common/moinmoin.png" alt="MoinMoin Logo">'
+    logo_string = u'<img src="/moin_static170/common/moinmoin.png" alt="MoinMoin Logo">'
 
     # name of entry page / front page [Unicode], choose one of those:
 
