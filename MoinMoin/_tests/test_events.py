7dfd9be8b211553d16e06318f512b28e9a5bf336
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-06-22 17:58:06 +0200

    Some unit test for the MoinMoin.events module.

---
+++ MoinMoin/_tests/test_events.py
@@ -0,0 +1,37 @@
+# -*- coding: iso-8859-1 -*-
+"""
+    MoinMoin - tests for MoinMoin.events module
+
+    @copyright: 2007 by Karol Nowak <grywacz@gmail.com>
+    @license: GNU GPL, see COPYING for details.
+"""
+
+from MoinMoin.request.CLI import Request
+import MoinMoin.events as events
+
+def test_get_handlers():
+    """Test if there are any event handlers. There should be some internal ones"""
+    
+    request = Request()
+    assert(events.get_handlers(request.cfg))
+
+def test_send_event():
+    """Test if event handlers are called and if proper messages are returned"""
+    
+    return_string = u"test_send_event"
+    
+    def event_handler(event):
+        return return_string
+    
+    request = Request()
+    request.cfg.event_handlers = [event_handler]
+    event = events.Event(request)
+    
+    print "A proper event handler should be called, and an 1-element list of messages returned"
+    assert(events.send_event(event)[0] == return_string)
+
+def test_subscribable_events():
+    """Test if there are any subscribable events. There should be some."""
+    
+    print "There should be at least a few subscribable events!"
+    assert(events.get_subscribable_events())
