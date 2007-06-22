e12504d070fc8d02435d053e64d558f93bf4dc20
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-06-22 18:54:13 +0200

    Fix issues pointed out by xorAxAx.

--- MoinMoin/_tests/test_events.py
+++ MoinMoin/_tests/test_events.py
@@ -9,13 +9,12 @@
 from MoinMoin.request.CLI import Request
 import MoinMoin.events as events
 
-def test_get_handlers():
+def test_get_handlers(request):
     """Test if there are any event handlers. There should be some internal ones"""
     
-    request = Request()
-    assert(events.get_handlers(request.cfg))
+    assert events.get_handlers(request.cfg)
 
-def test_send_event():
+def test_send_event(request):
     """Test if event handlers are called and if proper messages are returned"""
     
     return_string = u"test_send_event"
@@ -23,15 +22,16 @@ def test_send_event():
     def event_handler(event):
         return return_string
     
-    request = Request()
     request.cfg.event_handlers = [event_handler]
     event = events.Event(request)
     
     print "A proper event handler should be called, and an 1-element list of messages returned"
-    assert(events.send_event(event)[0] == return_string)
+    assert events.send_event(event)[0] == return_string
 
-def test_subscribable_events():
+def test_subscribable_events(request):
     """Test if there are any subscribable events. There should be some."""
     
     print "There should be at least a few subscribable events!"
-    assert(events.get_subscribable_events())
+    assert events.get_subscribable_events()
+    
+coverage_modules = ["MoinMoin.events"]
