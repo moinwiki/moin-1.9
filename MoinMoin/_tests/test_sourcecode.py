d3309249fec4e8356250180f7914d9ef2ab3911c
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2007-07-15 14:05:12 +0200

    test_sourcecode: only test recently changed stuff, use a single os.stat call's results

--- MoinMoin/_tests/test_sourcecode.py
+++ MoinMoin/_tests/test_sourcecode.py
@@ -8,7 +8,7 @@ Additionally, we check that the files have no crlf (Windows style) line endings.
 @license: MIT licensed
 """
 
-import os, re
+import os, re, time, stat
 
 import pep8
 
@@ -29,6 +29,14 @@ TRAILING_SPACES = 'nochange' # 'nochange' or 'fix'
                              # use 'fix' with extreme caution and in a separate changeset!
 FIX_TS_RE = re.compile(r' +$', re.M) # 'fix' mode: everything matching the trailing space re will be removed
 
+
+RECENTLY = time.time() - 7 * 24*60*60 # we only check stuff touched recently.
+# After doing a fresh clone, this procedure is recommended:
+# 1. Run the tests once to see if everything is OK (as cloning updates the mtime,
+#    it will test every file).
+# 2. Before starting to make new changes, use "touch" to change all timestamps
+#    to a time before <RECENTLY>.
+# 3. Regularly run the tests, they will run much faster now.
 
 def pep8_error_count(path):
     # process_options initializes some data structures and MUST be called before each Checker().check_all()
@@ -59,10 +67,12 @@ def test_sourcecode():
             path = os.path.join(ROOT, *reldir.split('/'))
         else:
             path = ROOT
-        if os.path.isfile(path):
-            if path.lower().endswith('.py'):
+        st = os.stat(path)
+        mode = st.st_mode
+        if stat.S_ISREG(mode): # is a regular file
+            if path.lower().endswith('.py') and st.st_mtime >= RECENTLY:
                 yield check_py_file, reldir, path
-        elif os.path.isdir(path):
+        elif stat.S_ISDIR(mode): # is a directory
             for entry in os.listdir(path):
                 if not entry.startswith('.'):
                     for _ in walk('%s/%s' % (reldir, entry)):
