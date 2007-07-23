1afeab6649b3f85ebcef8f3bc17d1eb3d0aad45b
Johannes Berg <johannes AT sipsolutions DOT net@localhost>
2007-07-23 12:57:53 +0200

    fix tests, using unicode instead of str

--- MoinMoin/parser/_tests/test_text_moin_wiki.py
+++ MoinMoin/parser/_tests/test_text_moin_wiki.py
@@ -180,11 +180,11 @@ class TestDateTimeMacro(ParserTestCase):
     needle = re.compile(text % r'(.+)')
     _tests = (
         # test                                   expected
-        ('[[DateTime(259200)]]',                '1970-01-04 00:00:00'),
-        ('[[DateTime(2003-03-03T03:03:03)]]',   '2003-03-03 03:03:03'),
-        ('[[DateTime(2000-01-01T00:00:00Z)]]',  '2000-01-01 00:00:00'), # works for Europe/Vilnius
-        ('[[Date(2002-02-02T01:02:03Z)]]',      '2002-02-02'),
-        ('[[DateTime(1970-01-06T00:00:00)]]',   '1970-01-06 00:00:00'), # fails e.g. for Europe/Vilnius
+        (u'[[DateTime(259200)]]',                '1970-01-04 00:00:00'),
+        (u'[[DateTime(2003-03-03T03:03:03)]]',   '2003-03-03 03:03:03'),
+        (u'[[DateTime(2000-01-01T00:00:00Z)]]',  '2000-01-01 00:00:00'), # works for Europe/Vilnius
+        (u'[[Date(2002-02-02T01:02:03Z)]]',      '2002-02-02'),
+        (u'[[DateTime(1970-01-06T00:00:00)]]',   '1970-01-06 00:00:00'), # fails e.g. for Europe/Vilnius
         )
 
     def setUp(self):
