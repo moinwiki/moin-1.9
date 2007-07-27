fe51b8e48c8050d20e0b003f22f7c1cb8bc2ba53
Johannes Berg <johannes AT sipsolutions DOT net@localhost>
2007-07-27 11:43:18 +0200

    rewrite action macro. this breaks some pages that'll need to be fixed

--- MoinMoin/macro/_tests/test_Action.py
+++ MoinMoin/macro/_tests/test_Action.py
@@ -53,11 +53,11 @@ class TestAction:
     def testActionCallingRaw(self):
         """ module_tested: executes raw by macro Action on existing page"""
 
-        expected = '<a href="./AutoCreatedMoinMoinTemporaryTestPageForAction?action=raw">raw</a>'
+        expected = '<a href="AutoCreatedMoinMoinTemporaryTestPageForAction?action=raw">raw</a>'
         text = '= title1 =\n||A||B||\n'
         self._createTestPage(text)
         m = self._make_macro()
-        result = Action.execute(m, 'raw')
+        result = Action.macro_Action(m, 'raw')
 
         assert result == expected
 
