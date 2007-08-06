a7c4a303ef555c8cf8c541ddcb87f20a18d24502
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-08-06 03:57:45 +0200

    Merge main.

--- MoinMoin/script/migration/_tests/test_conv160_wiki.py
+++ MoinMoin/script/migration/_tests/test_conv160_wiki.py
@@ -5,7 +5,6 @@
     TODO:
     * add some ../some_page test
     * add some /some_page test
-    * add more quote_triggers
     * fix parser/converter anchor link handling
     * emit a warning if we find some page name that was renamed as a macro argument?
     * shall we support camelcase renaming?
@@ -36,6 +35,20 @@ def test_wiki_conversion(request):
         # NEEDED? ('CamelCase', {}, 'CamelCase'),
         # FAILS ('RenameThis', rename_some_page, 'ThisRenamed'),
         # NEEDED? ('!RenameThis', {}, '!RenameThis'), # not a link
+
+        # FAILING tests:
+        #('[wiki:/OtherPage]', rename_some_page, '[wiki:/OtherPage]'),
+        #('[wiki:/OtherPage other page]', rename_some_page, '[wiki:/OtherPage other page]'),
+        # ('[:Something:Something]', {}, '["Something"]'),
+        #('[attachment:My%20Attachment.jpg:it works]', {}, '[attachment:"My Attachment.jpg" it works]'),
+        #('[wiki:LinuxWiki: LinuxWiki.de]', {}, '[wiki:LinuxWiki: LinuxWiki.de]'),
+        #('[:MeatBall:CleanLinking meatball-wiki: clean linking]', {}, '[:MeatBall:CleanLinking meatball-wiki: clean linking]'),
+
+        # ambiguity!!! can be resolved with some interwiki map lookup
+        # and transformed to wiki:SeaPig:BrianDorsey if SeaPig is in 
+        # interwiki map, but no page SeaPig exists.
+        #('[wiki:MacroMarket/EmbedObject EO]', {}, '["MacroMarket/EmbedObject" EO]'),
+        ('[wiki:SeaPig/BrianDorsey]', {}, '[wiki:SeaPig:BrianDorsey]'),
 
         # "nothing changed" checks
         ('', {}, ''),
