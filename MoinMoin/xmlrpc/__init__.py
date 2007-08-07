6bab4f6c2103b74ead259b38a5490ac2f1e4369c
Karol 'grzywacz' Nowak <grzywacz@sul.uni.lodz.pl>
2007-08-08 00:40:21 +0200

    More whitespace fixes.

--- MoinMoin/xmlrpc/__init__.py
+++ MoinMoin/xmlrpc/__init__.py
@@ -567,12 +567,12 @@ class XmlRpcBase:
         return [(self._outstr(hit.page_name),
                  self._outstr(results.formatContext(hit, 180, 1)))
                 for hit in results.hits]
-        
+
     def xmlrpc_searchPagesEx(self, query_string, search_type, length, case, mtime, regexp):
         """ Searches pages for query_string - extended version for compatibility
-        
+
         This function, in contrary to searchPages(), doesn't return HTML-formatted data.
-        
+
         @param query_string: term to search for
         @param search_type: "text" or "title" search
         @param length: length of context preview (in characters)
@@ -580,7 +580,7 @@ class XmlRpcBase:
         @param mtime: only output pages modified after mtime
         @param regexp: should the query_string be treates as a regular expression?
         @return: (page name, context preview, page url)
-        
+
         """
         from MoinMoin import search
         from MoinMoin.formatter.text_plain import Formatter
@@ -588,7 +588,7 @@ class XmlRpcBase:
         kwargs = {"sort": "page_name", "case": case, "regex": regexp}
         if search_type == "title":
             kwargs["titlesearch"] = True
-            
+
         results = search.searchPages(self.request, query_string, **kwargs)
         results.formatter = Formatter(self.request)
         results.request = self.request
@@ -596,7 +596,7 @@ class XmlRpcBase:
         return [(self._outstr(hit.page_name),
                  self._outstr(results.formatContext(hit, length, 1)),
                  self.request.getQualifiedURL(hit.page.url(self.request, {}, relative=False)))
-                for hit in results.hits]            
+                for hit in results.hits]
 
     def xmlrpc_getMoinVersion(self):
         """ Returns a tuple of the MoinMoin version:
