36ff364bec8de942f28252df277c5c7b0dd580c3
TW <tw@waldmann-edv.de>
2007-08-26 13:52:35 +0200

    simple po file conversion to new (macro) markup

---
+++ MoinMoin/i18n/tools/markup15to16.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+"""
+    convert some markup contained in po files to new link/macro markup
+"""
+DOMAIN = "MoinMoin"
+
+import re
+import sys
+import codecs
+
+def run():
+    sys.path.insert(0, '../..')
+
+    langfname = sys.argv[1]
+    lang = langfname.replace('_', '-') # module names use _ instead of -
+
+    f = codecs.open("%s.%s.po" % (langfname, DOMAIN), "r", "utf-8")
+    text = f.read()
+    f.close()
+
+    # replace [[Macro(...)]] by <<Macro(...)>>
+    macro_rule = r"\[\[(?P<macro>.*?)\]\]"
+    macro_repl = r"<<\g<macro>>>"
+    macro_re = re.compile(macro_rule, re.U)
+    text = macro_re.sub(macro_repl, text)
+
+    #print repr(text)
+
+    f = codecs.open("%s.%s.po" % (langfname, DOMAIN), "w", "utf-8")
+    f.write(text)
+    f.close()
+
+if __name__ == "__main__":
+    run()
+
