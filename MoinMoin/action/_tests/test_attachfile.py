969fef4ba7422249a15d1703b7b2927486c22d2f
ReimarBauer <rb.proj@gmail.com>
2007-10-03 10:47:31 +0200

    test_Attachfile: getFilename checked for creating attachments dir

--- MoinMoin/action/_tests/test_attachfile.py
+++ MoinMoin/action/_tests/test_attachfile.py
@@ -3,10 +3,11 @@
     MoinMoin - tests of AttachFile action
 
     @copyright: 2007 by Karol Nowak <grywacz@gmail.com>
+                     MoinMoin:ReimarBauer
     @license: GNU GPL, see COPYING for details.
 """
-
-from MoinMoin.action.AttachFile import add_attachment, exists
+import os
+from MoinMoin.action import AttachFile
 from MoinMoin.PageEditor import PageEditor
 from MoinMoin._tests.common import gain_superuser_rights
 
@@ -22,7 +23,24 @@ def test_add_attachment(request):
     editor.saveText("Test text!", 0)
 
     print "First of all, no exceptions should be raised!"
-    add_attachment(request, pagename, filename, "Test content", True)
+    AttachFile.add_attachment(request, pagename, filename, "Test content", True)
 
     print "The save attachment should actually exist!"
-    assert exists(request, pagename, filename)
+    assert AttachFile.exists(request, pagename, filename)
+
+def test_get_attachment_path_created_on_getFilename(request):
+    """
+    Tests if AttachFile.getFilename creates the attachment dir on requesting
+    """
+    pagename = "ThisPageDoesOnlyExistForThisTest"
+    filename = ""
+    result = os.path.exists(AttachFile.getFilename(request, pagename, filename))
+    expect = True
+
+    # real delete pagename from filesystem
+    import shutil
+    page = PageEditor(request, pagename, do_editor_backup=0)
+    fpath = page.getPagePath(check_create=0)
+    shutil.rmtree(fpath, True)
+
+    assert expect == result
