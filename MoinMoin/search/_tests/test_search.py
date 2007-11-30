700c92c5d432103ea292669788e2afcecf825a7a
TW <tw@waldmann-edv.de>
2007-11-30 14:58:20 +0100

    search: add test for title search with negative term (currently failing)

--- MoinMoin/search/_tests/test_search.py
+++ MoinMoin/search/_tests/test_search.py
@@ -6,8 +6,6 @@
     @license: GNU GPL, see COPYING for details.
 """
 
-import pprint
-
 from MoinMoin import search
 
 
@@ -35,17 +33,34 @@ class TestQueryParsing:
     """ search: query parser tests """
 
     def testQueryParser(self):
-        """ search: ... """
+        """ search: test the query parser """
         parser = search.QueryParser()
         for query, wanted in [
             ("a", '"a"'),
             ("a b", '["a" "b"]'),
             ("a -b c", '["a" -"b" "c"]'),
             ("aaa bbb -ccc", '["aaa" "bbb" -"ccc"]'),
+            ("title:aaa title:bbb -title:ccc", '[!"aaa" !"bbb" -!"ccc"]'),
             ("aaa OR bbb", '["aaa" "OR" "bbb"]'),
             ]:
             result = parser.parse_query(query)
             assert str(result) == wanted
+
+
+class TestSearch:
+    """ search: test search """
+
+    def testTitleSearchFrontPage(self):
+        """ search: title search for FrontPage """
+        query, wanted  = "title:FrontPage", 1
+        result = search.searchPages(self.request, query)
+        assert len(result.hits) == wanted
+
+    def testTitleSearchNegativeTerm(self):
+        """ search: title search for FrontPage """
+        helpon_count = len(search.searchPages(self.request, "title:HelpOn").hits)
+        result = search.searchPages(self.request, "title:HelpOn -title:Acl")
+        assert len(result.hits) == helpon_count - 1
 
 
 coverage_modules = ['MoinMoin.search']
