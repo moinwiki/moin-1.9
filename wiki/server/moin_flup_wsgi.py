d37d1a05efdc1e135fc8b2fbac7a99483ff22995
TW <tw@waldmann-edv.de>
2008-01-06 20:06:00 +0100

    WSGI: initialize logging (untested) (port from 1.6)

--- wiki/server/moin_flup_wsgi.py
+++ wiki/server/moin_flup_wsgi.py
@@ -1,12 +1,16 @@
 """
-    MoinMoin - WSGI application
+    MoinMoin - Moin as WSGI application with flup as fcgi gateway
 
-    @copyright: 2005 by Anakim Border <akborder@gmail.com>
+    @copyright: 2005 by Anakim Border <akborder@gmail.com>,
+                2007 by MoinMoin:ThomasWaldmann
     @license: GNU GPL, see COPYING for details.
 """
 
 use_threads = True
 unixSocketPath = '/tmp/moin.sock'
+
+import os
+import logging
 
 # Set threads flag, so other code can use proper locking
 from MoinMoin import config
@@ -14,8 +18,13 @@ config.use_threads = use_threads
 del config
 
 from flup.server.fcgi import WSGIServer
-from MoinMoin.server.server_wsgi import moinmoinApp
-import os
+from MoinMoin.server.server_wsgi import moinmoinApp, WsgiConfig
+
+class Config(WsgiConfig):
+    logPath = 'moin.log' # adapt to your needs!
+    #loglevel_file = logging.INFO # adapt to your needs!
+
+config = Config() # MUST create an instance to init logging
 
 if __name__ == '__main__':
     server = WSGIServer(moinmoinApp, bindAddress=unixSocketPath)
