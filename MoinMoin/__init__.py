ceb0c50f6be045c72b144d8dd11a5d7f4869d62d
TW <tw@waldmann-edv.de>
2008-03-30 04:30:24 +0200

    updating some (c) lines and docs/CHANGES

--- MoinMoin/__init__.py
+++ MoinMoin/__init__.py
@@ -3,7 +3,7 @@
 MoinMoin Version 1.7.0alpha
 
 @copyright: 2000-2006 by Juergen Hermann <jh@web.de>,
-            2002-2007 MoinMoin:ThomasWaldmann
+            2002-2008 MoinMoin:ThomasWaldmann
 @license: GNU GPL, see COPYING for details.
 """
 
