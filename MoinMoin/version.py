c0feb2c25197a373668400c55e0a0ca458f5f7b7
TW <tw@waldmann-edv.de>
2008-04-26 23:16:48 +0200

    preparing 1.7.0beta1 release: bumping version, updating CHANGES

--- MoinMoin/version.py
+++ MoinMoin/version.py
@@ -15,7 +15,7 @@ except:
     patchlevel = 'release'
 
 project = "MoinMoin"
-release = '1.7.0alpha'
+release = '1.7.0beta1'
 release_short = '170' # used for url_prefix_static
 revision = patchlevel
 
