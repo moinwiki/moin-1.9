7126c0512002cb7e7e787a7d94e0345ddee14621
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2008-05-06 17:51:45 +0200

    events.wikidictsrescan: handle TrivialPageChangedEvent too (bug fix for MoinMoinBugs/17devWikiDictsDontUpdate)

--- MoinMoin/events/wikidictsrescan.py
+++ MoinMoin/events/wikidictsrescan.py
@@ -16,7 +16,8 @@ from MoinMoin import wikidicts
 
 def handle(event):
     # "changed" includes creation, deletion, renamed and copied
-    if isinstance(event, ev.PageChangedEvent) or isinstance(event, ev.PageRenamedEvent) or isinstance(event, ev.PageCopiedEvent):
+    if (isinstance(event, ev.PageChangedEvent) or isinstance(event, ev.PageRenamedEvent) or
+        isinstance(event, ev.PageCopiedEvent) or isinstance(event, ev.TrivialPageChangedEvent)):
         cfg = event.request.cfg
         pagename = event.page.page_name
         if cfg.cache.page_dict_regex.search(pagename) or \
