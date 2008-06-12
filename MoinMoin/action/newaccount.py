eb9538a83fced761aa5151864a1b87a5edd1c3c5
Johannes Berg <johannes AT sipsolutions DOT net@localhost>
2008-06-12 16:56:20 +0200

    make recoverpass/newaccount refuse access unless MoinAuth is enabled

--- MoinMoin/action/newaccount.py
+++ MoinMoin/action/newaccount.py
@@ -10,6 +10,7 @@ from MoinMoin import user, wikiutil, util
 from MoinMoin.Page import Page
 from MoinMoin.widget import html
 from MoinMoin.security.textcha import TextCha
+from MoinMoin.auth import MoinAuth
 
 
 _debug = False
@@ -158,7 +159,17 @@ def _create_form(request):
     return unicode(ret)
 
 def execute(pagename, request):
-    pagename = pagename
+    found = False
+    for auth in request.cfg.auth:
+        if isinstance(auth, MoinAuth):
+            found = True
+            break
+
+    if not found:
+        # we will not have linked, so forbid access
+        request.makeForbidden403()
+        return
+
     page = Page(request, pagename)
     _ = request.getText
     form = request.form
