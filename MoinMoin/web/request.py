a0bd814b01ea26fc518a06bf444691622c1057cc
Florian Krupicka <florian.krupicka@googlemail.com>
2008-06-19 17:37:53 +0200

    Consolidate Request and Response into single object

--- MoinMoin/web/request.py
+++ MoinMoin/web/request.py
@@ -10,17 +10,44 @@ import re
 
 from werkzeug.wrappers import Request as WerkzeugRequest
 from werkzeug.wrappers import Response as WerkzeugResponse
+from werkzeug.utils import EnvironHeaders, cached_property
 
 from MoinMoin import config
 
 from MoinMoin import log
 logging = log.getLogger(__name__)
 
-class Request(WerkzeugRequest):
+class Request(WerkzeugRequest, WerkzeugResponse):
+    """ A full featured Request/Response object.
+
+    To better distinguish incoming and outgoing data/headers,
+    incoming versions are prefixed with 'in_' in contrast to
+    original Werkzeug implementation.
+    """
     charset = config.charset
     encoding_errors = 'replace'
-
-class Response(WerkzeugResponse):
-    charset = config.charset
     default_mimetype = 'text/html'
-    
+
+    def __init__(self, environ, populate_request=True, shallow=False,
+                 response=None, status=None, headers=None, mimetype=None,
+                 content_type=None):
+        WerkzeugRequest.__init__(self, environ, populate_request, shallow)
+        WerkzeugResponse.__init__(self, response, status, headers,
+                                  mimetype, content_type)
+
+    data = WerkzeugResponse.data
+    stream = WerkzeugResponse.stream
+
+    def in_headers(self):
+        return EnvironHeaders(self.environ)
+    in_headers = cached_property(in_headers, doc=WerkzeugRequest.headers.__doc__)
+
+    def in_stream(self):
+        if self._data_stream is None:
+            self._load_form_data()
+        return self._data_stream
+    in_stream = property(in_stream, doc=WerkzeugRequest.stream.__doc__)
+
+    def in_data(self):
+        return self.in_stream.read()
+    in_data = cached_property(in_data, doc=WerkzeugRequest.data.__doc__)
