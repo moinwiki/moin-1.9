51052cf218a22b1255c93130e1338aa863582784
Florian Krupicka <florian.krupicka@googlemail.com>
2008-08-02 22:12:18 +0200

    Fixed: variable reference and left out request handler

--- MoinMoin/web/serving.py
+++ MoinMoin/web/serving.py
@@ -68,10 +68,11 @@ def run_server(host='localhost', port=8080, docs='/usr/share/moin/htdocs',
     from MoinMoin.wsgiapp import application
 
     if docs and os.path.isdir(docs):
-        shared = {url_prefix_static: docs,
+        shared = {config.url_prefix_static: docs,
                   '/favicon.ico': os.path.join(docs, 'favicon.ico'),
                   '/robots.txt': os.path.join(docs, 'robots.txt')}
         application = SharedDataMiddleware(application, shared)
 
     run_simple(host, port, application, threaded=threaded,
-               use_debugger=use_debugger)
+               use_debugger=use_debugger,
+               request_handler=RequestHandler)
