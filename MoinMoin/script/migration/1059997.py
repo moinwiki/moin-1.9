89a2deaa10f109089e4840d4a9119e174853c590
Johannes Berg <johannes AT sipsolutions DOT net@localhost>
2008-08-05 17:51:08 +0200

    fix pep8

--- MoinMoin/script/migration/1059997.py
+++ MoinMoin/script/migration/1059997.py
@@ -6,7 +6,7 @@
     Note: this is a special hack for some users of a early 1.6 alpha version,
           this code is skipped and NOT executed in a normal release-to-release
           migration (like when going from 1.5.x release to 1.6.0 release).
-          
+
           If you run this early 1.6alpha code (with different link markup than
           1.5.x AND 1.6.x release has), you need to manually put 1059997 into
           your data/meta file to have this special code executed.
