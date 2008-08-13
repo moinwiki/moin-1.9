7eef45da913247a7b7770fc2395c8edd3b316b97
Florian Krupicka <florian.krupicka@googlemail.com>
2008-08-13 21:06:45 +0200

    Fixup sys.path to accomodate for complete libraries under MoinMoin.support

--- MoinMoin/web/__init__.py
+++ MoinMoin/web/__init__.py
@@ -8,3 +8,26 @@
     @copyright: 2008-2008 MoinMoin:FlorianKrupicka
     @license: GNU GPL, see COPYING for details.
 """
+
+def _fixup_deps():
+    """
+    Alter the system path to import some 3rd party dependencies from
+    inside the MoinMoin.support package. This is meant for deps
+    used inside this package, which are mainly werkzeug and flup.
+    """
+    import sys, os
+    from MoinMoin import support
+    dirname = os.path.dirname(support.__file__)
+    dirname = os.path.abspath(dirname)
+    found = False
+    for path in sys.path:
+        if os.path.abspath(path) == dirname:
+            found = True
+            break
+    if not found:
+        sys.path.append(dirname)
+
+try:
+    _fixup_deps()
+finally:
+    del _fixup_deps
