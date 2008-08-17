c3a3429d4e26513ad5760b99aad6335a501d40f7
Florian Krupicka <florian.krupicka@googlemail.com>
2008-08-18 00:06:47 +0200

    Code review: updated doc strings, more verbosity

--- MoinMoin/web/__init__.py
+++ MoinMoin/web/__init__.py
@@ -1,9 +1,14 @@
 # -*- coding: iso-8859-1 -*-
 """
-    MoinMoin - This package contains the interface between webserver
-               and application. This is meant to become a replacement
-               and/or port of code currently scattered in MoinMoin.request
-               and MoinMoin.server.
+    MoinMoin - Low-level interface code between the wiki and the web
+
+    This package contains everything related to interfacing the wiki with
+    the actual request from the web. It replaces the former packages
+    `MoinMoin.request` and `MoinMoin.server`. There is code for convenient
+    access to the supplied request parameters (using the werkzeug library),
+    wrappers (called contexts) that try to capture the use of the former
+    Request-objects in MoinMoin, session handling and interfaces to
+    common webserver deployment methods.
 
     @copyright: 2008-2008 MoinMoin:FlorianKrupicka
     @license: GNU GPL, see COPYING for details.
