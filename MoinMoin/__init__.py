40e748b3ece14948d19ce92b20094b9ef4c4fd59
TW <tw@waldmann-edv.de>
2008-09-14 16:57:53 +0200

    bumping version number to 1.8.0beta1

--- MoinMoin/__init__.py
+++ MoinMoin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-MoinMoin Version 1.8.0alpha
+MoinMoin Version 1.8.0beta
 
 @copyright: 2000-2006 by Juergen Hermann <jh@web.de>,
             2002-2008 MoinMoin:ThomasWaldmann
