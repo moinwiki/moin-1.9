829b50292069aa37ec0be3654780454c3fb1d02e
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2008-09-21 23:08:58 +0200

    bump version number to 1.8.0beta2

--- MoinMoin/version.py
+++ MoinMoin/version.py
@@ -15,7 +15,7 @@ except:
     patchlevel = 'release'
 
 project = "MoinMoin"
-release = '1.8.0beta1'
+release = '1.8.0beta2'
 release_short = '180' # used for url_prefix_static
 revision = patchlevel
 
