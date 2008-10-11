4fbc653923ac50b24519223e65b9e247eb93ac5b
TW <tw@waldmann-edv.de>
2008-10-12 00:26:49 +0200

    replace the diff parser by a thin wrapper around the highlight parser (to keep syntax like {{{#!diff ... working)

--- MoinMoin/parser/text_diff.py
+++ MoinMoin/parser/text_diff.py
@@ -1,39 +1,26 @@
 # -*- coding: iso-8859-1 -*-
 """
-    MoinMoin - Diff Parser - highlights diff tool output
+    MoinMoin - diff/patch Parser
+    
+    DEPRECATED compatibility wrapper calling the highlight parser.
+    
+    This is to support (deprecated) existing syntax like:
+    {{{#!diff ...
+    ...
+    }}}
 
-    @copyright: 2006 Emilio Lopes, inspired by previous work
-                done by Fabien Ninoles and Juergen Hermann
+    It is equivalent to the new way to highlight code:
+    {{{#!highlight diff ...
+    ...
+    }}}
+
+    @copyright: 2008 MoinMoin:ThomasWaldmann
     @license: GNU GPL, see COPYING for details.
 """
 
-from MoinMoin.parser._ParserBase import ParserBase
+from MoinMoin.parser.highlight import Parser as HighlightParser
+from MoinMoin.parser.highlight import Dependencies
 
-class Parser(ParserBase):
-    parsername = "ColorizedDiff"
-    extensions = ['.diff', '.patch', ]
-    Dependencies = []
-
-    def setupRules(self):
-        ParserBase.setupRules(self)
-
-        self.addRule("Comment", r'^(diff .*?)$')
-        self.addRule("Comment", r'^(\*\*\* .*?)$')
-        self.addRule("Comment", r'^(--- .*?)$')
-        self.addRule("Comment", r'^(\+\+\+ .*?)$')
-        self.addRule("Comment", r'^\*\*\*\*\*\*\*\*\*\*\*\*\*\*\* *$')
-
-        self.addRule("DiffSeparator", r'^(@@ .*?)$')
-        self.addRule("DiffSeparator", r'^--- *$')
-
-        self.addRule("DiffAdded", r'^(\+.*?)$')
-        self.addRule("DiffRemoved", r'^(-.*?)$')
-        self.addRule("DiffAdded", r'^(>.*?)$')
-        self.addRule("DiffRemoved", r'^(<.*?)$')
-        self.addRule("DiffChanged", r'^(!.*?)$')
-
-        self.addRuleFormat("DiffAdded")
-        self.addRuleFormat("DiffRemoved")
-        self.addRuleFormat("DiffChanged")
-        self.addRuleFormat("DiffSeparator")
+class Parser(HighlightParser):
+    parsername = 'diff'  # Lexer name pygments recognizes
 
