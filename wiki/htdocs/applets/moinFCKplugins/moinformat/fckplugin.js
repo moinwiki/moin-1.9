cc34f1896f9105655adbb7eba8577eeb79fb1ac1
TW <tw@waldmann-edv.de>
2008-11-08 14:43:49 +0100

    GUI editor: fix heading levels when inserting new headings (thanks to PrebenR)

--- wiki/htdocs/applets/moinFCKplugins/moinformat/fckplugin.js
+++ wiki/htdocs/applets/moinFCKplugins/moinformat/fckplugin.js
@@ -36,13 +36,14 @@ MoinToolbarFontFormatCombo.prototype.CreateItems = function(targetSpecialCombo)
   // Get the format names from the language file.
   var aNames = FCKLang['FontFormats'].split(';');
   var oNames = {
-    p  : aNames[0],
-    pre  : aNames[1],
-    h2  : aNames[3], // h2 as Title 1
-    h3  : aNames[4], // and so on
-    h4  : aNames[5],
-    h5  : aNames[6],
-    h6  : aNames[7]
+    p   : aNames[0],
+    pre : aNames[1],
+    h1  : aNames[3], // h1 as Title 1, aNames[2] is address
+    h2  : aNames[4], // and so on
+    h3  : aNames[5], 
+    h4  : aNames[6],
+    h5  : aNames[7],
+    h6  : aNames[8]
   };
 
   // Get the available formats from the configuration file.
