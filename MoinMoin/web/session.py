0a8c78904fe0991a5f452c4d3de21e2220c689ac
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2009-02-02 05:54:21 +0100

    new cfg.session_dir setting, store sessions into cache_dir/__session__ by default

--- MoinMoin/web/session.py
+++ MoinMoin/web/session.py
@@ -16,6 +16,7 @@ from werkzeug.utils import dump_cookie
 from werkzeug.contrib.sessions import FilesystemSessionStore, Session
 
 from MoinMoin import caching
+from MoinMoin.util import filesys
 
 from MoinMoin import log
 logging = log.getLogger(__name__)
@@ -55,21 +56,33 @@ class FileSessionService(SessionService):
     inherited from the builtin `dict`)
     """
     def __init__(self, cookie_name='MOIN_SESSION'):
-        self.store = FilesystemSessionStore(session_class=MoinSession)
         self.cookie_name = cookie_name
+        self.store = None
+
+    def _store_get(self, request):
+        if self.store is None:
+            path = request.cfg.session_dir
+            try:
+                filesys.mkdir(path)
+            except OSError:
+                pass
+            self.store = FilesystemSessionStore(path=path, filename_template='%s', session_class=MoinSession)
+        return self.store
 
     def get_session(self, request, sid=None):
         if sid is None:
             sid = request.cookies.get(self.cookie_name, None)
+        store = self._store_get(request)
         if sid is None:
-            session = self.store.new()
+            session = store.new()
         else:
-            session = self.store.get(sid)
+            session = store.get(sid)
         return session
 
     def destroy_session(self, request, session):
         session.clear()
-        self.store.delete(session)
+        store = self._store_get(request)
+        store.delete(session)
 
     def finalize(self, request, session):
         if request.user.auth_method == 'setuid':
@@ -105,5 +118,6 @@ class FileSessionService(SessionService):
             request.headers.add('Set-Cookie', cookie)
 
         if session.should_save:
-            self.store.save(session)
+            store = self._store_get(request)
+            store.save(session)
 
