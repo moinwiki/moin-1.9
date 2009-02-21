405b993fa44dbc3c21f685849569d8dc33bae99c
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2009-02-21 17:14:29 +0100

    packages.do_ensureversion: get only the numbers of the release string (ignore betaX)

--- MoinMoin/packages.py
+++ MoinMoin/packages.py
@@ -7,7 +7,7 @@
     @license: GNU GPL, see COPYING for details.
 """
 
-import os, sys
+import os, re, sys
 import zipfile
 
 from MoinMoin import config, wikiutil, caching, user
@@ -201,7 +201,8 @@ class ScriptEngine:
 
         from MoinMoin.version import release
         version_int = [int(x) for x in version.split(".")]
-        release = [int(x) for x in release.split(".")]
+        # use a regex here to get only the numbers of the release string (e.g. ignore betaX)
+        release = re.compile('\d+').findall(release)[0:3]
         if version_int > release:
             if lines > 0:
                 self.goto = lines
