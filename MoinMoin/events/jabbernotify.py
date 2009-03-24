323a8d60b8158d8573280fbcc8e44f03f526dc53
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2009-03-24 22:10:41 +0100

    fix wrong links in attachment notifications

--- MoinMoin/events/jabbernotify.py
+++ MoinMoin/events/jabbernotify.py
@@ -74,7 +74,7 @@ def handle_file_attached(event):
     for lang in subscribers:
         recipients.extend(subscribers[lang])
 
-    attachlink = request.getBaseURL() + getAttachUrl(event.pagename, event.filename, request)
+    attachlink = request.getQualifiedURL(getAttachUrl(event.pagename, event.filename, request))
     pagelink = request.getQualifiedURL(page.url(request, {}))
 
     for lang in subscribers.keys():
