66020d5532711b05298fa0504ee4a4b865f9c5cc
Dmitrijs Milajevs <dimazest@gmail.com>
2009-06-27 13:07:36 +0200

    Groups2009: script.account.homepage was updated to use new request.groups.

--- MoinMoin/script/account/homepage.py
+++ MoinMoin/script/account/homepage.py
@@ -6,7 +6,7 @@ MoinMoin - creates user homepage for existing accounts
 @license: GNU GPL, see COPYING for details.
 """
 
-from MoinMoin import user, wikidicts
+from MoinMoin import user
 from MoinMoin.Page import Page
 from MoinMoin.PageEditor import PageEditor
 from MoinMoin.script import MoinScript
@@ -108,8 +108,7 @@ CategoryHomepage
         if self.options.user_homepage:
             members = [self.options.user_homepage, ]
         elif self.options.name_of_group_page:
-            user_group = wikidicts.Group(request, self.options.name_of_group_page)
-            members = user_group.members()
+            members = request.groups.get(self.options.name_of_group_page, [])
         elif self.options.all_users:
             uids = user.getUserList(request)
             members = [user.User(request, uid).name for uid in uids]
