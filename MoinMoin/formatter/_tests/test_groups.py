0ee7616a1c4aac4074a0dec952e814c3e1614574
Dmitrijs Milajevs <dimazest@gmail.com>
2009-07-14 09:26:14 +0200

    Groups2009: formatter.groups for links collects links, not labels. Creole tets for a MoinMoin.formatter.groups

--- MoinMoin/formatter/_tests/test_groups.py
+++ MoinMoin/formatter/_tests/test_groups.py
@@ -10,7 +10,7 @@ from  MoinMoin.formatter.groups import Formatter
 from MoinMoin.Page import Page
 from MoinMoin._tests import become_trusted, create_page, nuke_page
 
-class TestGroupFormatter(object):
+class TestGroupFormatterWikiMarkup(object):
 
     def get_members(self, text):
         request = self.request
@@ -54,7 +54,7 @@ class TestGroupFormatter(object):
 """
         members = self.get_members(text)
         assert len(members) == 1
-        assert u'label' in members
+        assert u'extended link' in members
 
     def test_extended_link_and_text(self):
         text = """
@@ -87,7 +87,7 @@ class TestGroupFormatter(object):
         text = """
     * first level
      * second level
-      * [[SomeLink]]
+      * [[SomeLink|label]]
        * forth level
         * and then some...
     * again first level
@@ -116,5 +116,112 @@ Ignore previous line and this text.
         assert len(members) == 1
         assert u'take this' in members
 
-coverage_modules = ['MoinMoin.formtter.groups']
 
+class TestGroupFormatterCreole(object):
+
+    def get_members(self, text):
+        request = self.request
+        formatter = Formatter(self.request)
+
+        become_trusted(request)
+        create_page(request, u'TestPageGroup', "#FORMAT creole \n" + text)
+        page = Page(request, 'TestPageGroup', formatter=formatter)
+        page.send_page(content_only=True)
+        nuke_page(request, u'TestPageGroup')
+
+        return formatter.members
+
+    def test_CamelCase(self):
+        text = """
+ * CamelCase
+"""
+        members = self.get_members(text)
+        assert len(members) == 1
+        assert u'CamelCase' in members
+
+    def test_extended_name(self):
+        text = """
+ * extended name
+"""
+        members = self.get_members(text)
+        assert len(members) == 1
+        assert u'extended name' in members
+
+    def test_extended_link(self):
+        text = """
+ * [[extended link]]
+"""
+        members = self.get_members(text)
+        assert len(members) == 1
+        assert u'extended link' in members
+
+    def test_extended_link_with_label(self):
+        text = """
+ * [[FrontPage|named link]]
+"""
+        members = self.get_members(text)
+        assert len(members) == 1
+        assert u'FrontPage' in members
+
+    def test_extended_link_and_text(self):
+        text = """
+ * [[extended link]] other text
+ * other text [[extended link]]
+ * other text [[extended link]] other text
+
+"""
+        members = self.get_members(text)
+        assert len(members) == 3
+        assert u'extended link other text' in members
+        assert u'other text extended link' in members
+        assert u'other text extended link other text' in members
+
+    def test_ignore_not_first_level_list(self):
+        text = """
+ * first level
+ ** second level
+ *** [[SomeLink]]
+ **** forth level
+ ***** and then some...
+ * again first level
+"""
+        members = self.get_members(text)
+        assert len(members) == 2
+        assert 'first level' in members
+        assert 'again first level' in members
+
+    def test_intended_list(self):
+        text = """
+    * first level
+    ** second level
+    *** [[SomeLink|label]]
+    **** forth level
+    ***** and then some...
+    * again first level
+"""
+        members = self.get_members(text)
+        assert len(members) == 2
+        assert 'first level' in members
+        assert 'again first level' in members
+
+    def test_ignore_other(self):
+        text = """
+= ignore this =
+ * take this
+
+Ignore previous line and this text.
+"""
+        members = self.get_members(text)
+        assert len(members) == 1
+        assert u'take this' in members
+
+    def test_strip_whitespace(self):
+        text = """
+ *   take this
+"""
+        members = self.get_members(text)
+        assert len(members) == 1
+        assert u'take this' in members
+
+
+coverage_modules = ['MoinMoin.formtter.groups']
