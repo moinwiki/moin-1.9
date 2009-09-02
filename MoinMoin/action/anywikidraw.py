5a522839e9db2b9da74608a03c9c29f85fc88dcf
ReimarBauer <rb.proj@gmail.com>
2009-09-02 17:10:39 +0200

    action anywikidraw: fixed url mapping bug in attachment_drawing

--- MoinMoin/action/anywikidraw.py
+++ MoinMoin/action/anywikidraw.py
@@ -83,10 +83,11 @@ def attachment_drawing(self, url, text, **kw):
         # XXX check how that is done by josef
         # we have a image map. inline it and add a map ref to the img tag
         mapid = 'ImageMapOf' + drawing
-        map = map.replace('%MAPNAME%', mapid)
+        map = map.replace('id="%s.svg"' % drawing, '')
+        map = map.replace('name="%s.svg"' % drawing, 'name="%s"' % mapid)
         # add alt and title tags to areas
-        map = re.sub(r'href\s*=\s*"((?!%ANYWIKIDRAW%).+?)"', r'href="\1" alt="\1" title="\1"', map)
-        map = map.replace('%ANYWIKIDRAW%"', '%s" alt="%s" title="%s"' % (drawing_url, title, title))
+        #map = re.sub(r'href\s*=\s*"((?!%TWIKIDRAW%).+?)"', r'href="\1" alt="\1" title="\1"', map)
+        #map = map.replace('%TWIKIDRAW%"', '%s" alt="%s" title="%s"' % (drawing_url, title, title))
         # unxml, because 4.01 concrete will not validate />
         map = map.replace('/>', '>')
         title = _('Clickable drawing: %(filename)s') % {'filename': self.text(drawing)}
