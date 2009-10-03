b40ea3b0d244370fb67219c5d2c5877eefd9255c
TW <tw@waldmann-edv.de>
2009-10-03 22:40:53 +0200

    merge moin/1.8

--- MoinMoin/action/RenamePage.py
+++ MoinMoin/action/RenamePage.py
@@ -72,7 +72,7 @@ class RenamePage(ActionBase):
     def do_action_finish(self, success):
         if success:
             url = Page(self.request, self.newpagename).url(self.request)
-            self.request.http_redirect(url, status='301')
+            self.request.http_redirect(url, code=301)
         else:
             self.render_msg(self.make_form(), "dialog")
 
