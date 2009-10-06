063807530fcd8820f4dee6eadec9d38283ea3a3b
TW <tw@waldmann-edv.de>
2009-10-06 21:31:57 +0200

    pdf indexing filter: use quiet operation to work around problems

--- MoinMoin/filter/application_pdf.py
+++ MoinMoin/filter/application_pdf.py
@@ -13,5 +13,9 @@
 from MoinMoin.filter import execfilter
 
 def execute(indexobj, filename):
-    return execfilter("pdftotext -enc UTF-8 %s -", filename)
+    # using -q switch to get quiet operation (no messages, no errors),
+    # because poppler-utils pdftotext on Debian/Etch otherwise generates
+    # lots of output on stderr (e.g. 10MB stderr output) and that causes
+    # problems in current execfilter implementation.
+    return execfilter("pdftotext -q -enc UTF-8 %s -", filename)
 
