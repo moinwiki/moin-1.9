509c1d0a1cf8d7027b5db7a07281312b91099493
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2009-10-07 18:47:05 +0200

    Xapian indexing: fix deadlocks, new MoinMoin.util.SubProcess module (see below)
    Due to the usage of multiple pipes (stdout, stderr) to the subprocess (usually
    a external filter program like pdftotext or antiword), deadlocks could occur
    (especially when the filter program wrote lots of output to stderr, larger than
    the buffer).
    
    This was (hopefully) fixed as follows:
    
    Python >= 2.4: use enhanced subclass of subprocess.Popen that implements a
    timeout - if the process did not end when the timeout is reached, it will get
    killed automatically.
    
    Note: there is windows code to support the timeout functionality, but only the
    linux code was tested. On windows with Python < 2.6, you need Python win32
    extensions to support process termination.
    
    Python 2.3: use popen2 to only get stdout (not stderr) - no support for:
     * timeout
     * stderr
     * rc
    Please use Python >= 2.4 if you have issues with this.
    We will drop the popen2 based code after requiring Python 2.4 with moin 1.9.

--- MoinMoin/filter/__init__.py
+++ MoinMoin/filter/__init__.py
@@ -9,12 +9,6 @@
 import sys, os
 import time
 
-try:
-    # requires Python >= 2.4
-    import subprocess
-except ImportError:
-    subprocess = None
-
 from MoinMoin import log
 logging = log.getLogger(__name__)
 
@@ -23,6 +17,23 @@ from MoinMoin.util import pysupport
 modules = pysupport.getPackageModules(__file__)
 
 standard_codings = ['utf-8', 'iso-8859-15', 'iso-8859-1', ]
+
+try:
+    # requires Python >= 2.4
+    from MoinMoin.util.SubProcess import exec_cmd
+except ImportError:
+    # Note: remove this after requiring 2.4 for moin
+    def exec_cmd(cmd, input=None, timeout=None):
+        child_stdin, child_stdout = os.popen2(cmd)
+        # Note: we do NOT use popen3 because it would cause
+        #       deadlocks with such simple code as below:
+        if input:
+            child_stdin.write(input)
+        data = child_stdout.read()
+        child_stdout.close()
+        errors = '' # we don't have it (see note above about popen3)
+        rc = 0 # we don't have it, TODO: check if we can get it somehow
+        return data, errors, rc
 
 
 def quote_filename(filename):
@@ -39,91 +50,14 @@ def quote_filename(filename):
     return filename
 
 
-def exec_cmd(cmd, timeout=300):
-    logging.debug("Trying to start command: %s" % cmd)
-    if subprocess:
-        tstart = time.time()
-        p = subprocess.Popen(cmd, shell=True,
-                             close_fds=not subprocess.mswindows,
-                             bufsize=1024,
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-        child_stdin, child_stdout, child_stderr = p.stdin, p.stdout, p.stderr
-
-        tmax = tstart + timeout
-        tnow = time.time()
-        data = []
-        errors = []
-        rc = None
-        while rc is None and tnow < tmax:
-            time.sleep(0.001) # wait at least 1ms between polls
-            data.append(child_stdout.read())
-            errors.append(child_stderr.read())
-            rc = p.poll()
-            tnow = time.time()
-
-        child_stdout.close()
-        child_stderr.close()
-
-        data = ''.join(data)
-        errors = ''.join(errors)
-
-        t = tnow - tstart
-        if rc is None:
-            logging.warning("Command '%s' timed out (%fs)" % (cmd, t))
-            rc = terminate(p)
-
-        if rc < 0:
-            logging.warning("Command '%s' was terminated by signal %d (%fs)" % (cmd, -rc, t))
-        elif rc > 0:
-            logging.warning("Command '%s' terminated with rc=%d (%fs)" % (cmd, rc, t))
-        else:
-            logging.debug("Command '%s' terminated normally rc=%d (%fs)" % (cmd, rc, t))
-
-    else:
-        child_stdin, child_stdout, child_stderr = os.popen3(cmd)
-        data = child_stdout.read()
-        errors = child_stderr.read()
-        child_stdout.close()
-        child_stderr.close()
-
-    logging.debug("Command '%s', stderr: %s, stdout: %d bytes" % (cmd, errors, len(data)))
-    return data, errors
-
-
-def terminate(proc):
-    """ subprocess.Popen.terminate is not implemented on some Windows python versions.
-        This workaround works on both POSIX and Windows.
-        Originally from: Guillaume Rava, http://code.activestate.com/recipes/576667/
-        terminate also seems to be only implemented in Python >= 2.6.
-    """
-    try:
-        proc.terminate()
-    except AttributeError:
-        if not subprocess.mswindows:
-            import signal
-            os.kill(proc.pid, signal.SIGTERM)
-        else:
-            try:
-                import win32api
-            except ImportError:
-                logging.warning("could not import win32api, please install win32 extensions")
-            else:
-                PROCESS_TERMINATE = 1
-                handle = win32api.OpenProcess(PROCESS_TERMINATE, False, proc.pid)
-                win32api.TerminateProcess(handle, -1)
-                win32api.CloseHandle(handle)
-    return proc.wait() # wait until it is really dead
-
-
 def execfilter(cmd, filename, codings=standard_codings):
     """ use cmd to get plaintext content of filename
         to decode to unicode, we use the first coding of codings list that
         does not throw an exception or force ascii
     """
     filter_cmd = cmd % quote_filename(filename)
-    data, errors = exec_cmd(filter_cmd)
+    data, errors, rc = exec_cmd(filter_cmd, timeout=300)
+    logging.debug("Command '%s', rc: %d, stdout: %d bytes, stderr: %s" % (filter_cmd, rc, len(data), errors))
     for c in codings:
         try:
             return data.decode(c)
