b4d927d84956e131a74a66cc3ff9694b883a987c
TW <tw@waldmann-edv.de>
2010-02-06 00:55:50 +0100

    merged moin/1.8

--- MoinMoin/script/migration/1080700.py
+++ MoinMoin/script/migration/1080700.py
@@ -1,6 +1,6 @@
 # -*- coding: iso-8859-1 -*-
 """
-    MoinMoin - migration from base rev 1080600
+    MoinMoin - migration from base rev 1080700
 
     Nothing to do, we just return the new data dir revision.
 
@@ -9,5 +9,5 @@
 """
 
 def execute(script, data_dir, rev):
-    return 1080700
+    return 1089999
 
