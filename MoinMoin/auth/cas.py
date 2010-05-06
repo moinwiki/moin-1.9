2663431fc620e104d0bff7085e5d38d5dc9e817e
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2010-05-06 15:36:56 +0200

    Fix auth.cas and userprefs.oidserv request object usage (untested, details see below)
    auth.cas
    ========
    request.getBaseURL() is deprecated, replaced it with request.url_root.
    Even better would be to replace it with request.abs_href(), if possible.
    
    request.getPathinfo() is gone, replaced it with request.path.
    Needs testing, esp. whether quote_plus and encode is still correct.
    
    In general: auth.cas needs an active maintainer!
    
    userprefs.oidserv
    =================
    request.getPathinfo() is gone, replaced it with request.path.
    Needs testing also (but should work, is done the same way at other places).
    
    In general: openid stuff needs an active maintainer!

--- MoinMoin/auth/cas.py
+++ MoinMoin/auth/cas.py
@@ -77,7 +77,7 @@ class CASAuth(BaseAuth):
         ticket = request.args.get('ticket')
         action = request.args.get("action", [])
         logoutRequest = request.args.get('logoutRequest', [])
-        url = request.getBaseURL() + urllib.quote_plus(request.getPathinfo().encode('utf-8'))
+        url = request.url_root + urllib.quote_plus(request.path.encode('utf-8'))
 
         # # handle logout request from CAS
         # if logoutRequest:
@@ -112,7 +112,7 @@ class CASAuth(BaseAuth):
 
     def logout(self, request, user_obj, **kw):
         if self.name and user_obj and user_obj.auth_method == self.name:
-            url = request.getBaseURL() + urllib.quote_plus(request.getPathinfo().encode('utf-8'))
+            url = request.url_root + urllib.quote_plus(request.path.encode('utf-8'))
             request.http_redirect(self.cas.logout_url(url))
 
             user_obj.valid = False
