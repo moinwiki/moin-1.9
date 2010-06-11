25350e8156f86a7a5c0fdcefb479c05fd50bfb03
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2010-06-11 20:51:01 +0200

    PEP8 whitespace fix

--- MoinMoin/theme/__init__.py
+++ MoinMoin/theme/__init__.py
@@ -1620,7 +1620,7 @@ var gui_editor_link_text = "%(text)s";
         page_find_page = wikiutil.getLocalizedPage(request, 'FindPage').page_name
         home_page = wikiutil.getInterwikiHomePage(request) # sorry theme API change!!! Either None or tuple (wikiname,pagename) now.
         page_parent_page = getattr(page.getParentPage(), 'page_name', None)
-        
+
         # set content_type, including charset, so web server doesn't touch it:
         request.content_type = "text/html; charset=%s" % (config.charset, )
 
