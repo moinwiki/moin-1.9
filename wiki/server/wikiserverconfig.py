0659414e159314bf6d597c8c85ea6877daeb2c73
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2010-10-03 22:29:11 +0200

    updated wikiserverconfig.py from internal defaults, sync the one in toplevel dir and the one in wiki/server/ - fixes hostname setting

--- wiki/server/wikiserverconfig.py
+++ wiki/server/wikiserverconfig.py
@@ -6,28 +6,19 @@ import os
 
 from MoinMoin.script.server.standalone import DefaultConfig
 
+
 class LocalConfig(DefaultConfig):
-    port = 8080 # if you use port < 1024, you need to start as root
-
-    # if you start the server as root, the standalone server can change
-    # to this user and group, e.g. 'www-data'
-    #user = ''
-    #group = ''
-
-    # use '' for all interface or "1.2.3.4" for some specific IP
-    #interface = 'localhost'
-
     # where the static data is served from - you can either use:
-    # docs = True  # serve the builtin static data from MoinMoin/web/static/htdocs
+    # docs = True  # serve the builtin static data from MoinMoin/web/static/htdocs/
     # docs = '/where/ever/you/like/to/keep/htdocs'  # serve it from the given path
     # docs = False  # do not serve static files at all (will not work except
     #               # you serve them in some other working way)
-    docs = True
+    #docs = True
 
-    # tuning options:
-    #serverClass = 'ThreadPoolServer'
-    #threadLimit = 10
-    #requestQueueSize = 50
+    # if you start the server as root, the standalone server can change
+    # to this user and group, e.g. 'www-data'
+    #user = None
+    #group = None
 
     # How to debug? Your options:
     # debug = 'off' # for production wikis, exceptions are logged
@@ -39,7 +30,37 @@ class LocalConfig(DefaultConfig):
     #          things from some debugger's web interface!
     # For convenience, the default behaviour (see below) is to read the
     # environment variable MOIN_DEBUGGER. If not set, it means the same as 'off'.
-    debug = os.environ.get('MOIN_DEBUGGER', 'off')
+    #debug = os.environ.get('MOIN_DEBUGGER', 'off')
+
+    # should the exception evaluation feature be enabled?
+    #use_evalex = True
+
+    # Werkzeug run_simple arguments below here:
+
+    # use '' for all interfaces or "1.2.3.4" for some specific IP
+    hostname = 'localhost'
+    # if you use port < 1024, you need to start as root
+    port = 8080
+
+    # either multi-thread or multi-process (not both):
+    # threaded = True, processes = 1 is usually what you want
+    # threaded = False, processes = 10 (for example) can be rather slow
+    # thus, if you need a forking server, maybe rather use apache/mod-wsgi!
+    #threaded = True
+    #processes = 1
+
+    # automatic code reloader - needs testing!
+    #use_reloader = False
+    #extra_files = None
+    #reloader_interval = 1
+
+    # we can't use static_files to replace our own middleware setup for moin's
+    # static files, because we also need the setup with other servers (like
+    # apache), not just when using werkzeug's run_simple server.
+    # But you can use it if you need to serve other static files you just need
+    # with the standalone wikiserver.
+    #static_files = None
+
 
 # DEVELOPERS! Do not add your configuration items there,
 # you could accidentally commit them! Instead, create a
