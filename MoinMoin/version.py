2d795c0545f2ce78b58cddf6c674b12be95aceb5
TW <tw@waldmann-edv.de>
2012-09-16 01:04:13 +0200

    bump version number to 1.9.5

--- MoinMoin/version.py
+++ MoinMoin/version.py
@@ -15,8 +15,8 @@ except:
     patchlevel = 'release'
 
 project = "MoinMoin"
-release = '1.9.4'
-release_short = '194' # used for url_prefix_static
+release = '1.9.5'
+release_short = '195' # used for url_prefix_static
 revision = patchlevel
 
 def update():
