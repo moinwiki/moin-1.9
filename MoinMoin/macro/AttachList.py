6262dfce597eabaa67e02a2b6bbbc3c1f3c2fd6d
Reimar Bauer <rb.proj AT googlemail DOT com@localhost>
2013-03-08 08:39:57 +0100

    AttachList: introduced search_term parameter (optional) for listing attachments by a regular expression

--- MoinMoin/macro/AttachList.py
+++ MoinMoin/macro/AttachList.py
@@ -9,16 +9,19 @@
     If mime_type isn't given, all files are listed.
 
     @copyright: 2004 Jacob Cohen, Nigel Metheringham,
-                2006 MoinMoin:ReimarBauer
+                2006-2013 MoinMoin:ReimarBauer
     @license: GNU GPL, see COPYING for details.
 """
 
+import re
+
 from MoinMoin.action.AttachFile import _build_filelist
 
-def macro_AttachList(macro, pagename=None, mime_type=u'*'):
+def macro_AttachList(macro, pagename=None, mime_type=u'*', search_term=u'.+'):
     # defaults if we don't get anything better
     if not pagename:
         pagename = macro.formatter.page.page_name
+    filterfn = re.compile(search_term, re.U).search
+    return _build_filelist(macro.request, pagename, 0, 1, mime_type=mime_type, filterfn=filterfn)
 
-    return _build_filelist(macro.request, pagename, 0, 1, mime_type=mime_type)
 
