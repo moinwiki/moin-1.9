0f76cb6d7ec88b83d3cab43cdf020e7066889a48
TW <tw@waldmann-edv.de>
2014-01-05 03:58:00 +0100

    update (c) year in some files, make pep8 checker happy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
     MoinMoin installer
 
     @copyright: 2001-2005 by Jürgen Hermann <jh@web.de>,
-                2006-2013 by MoinMoin:ThomasWaldmann
+                2006-2014 by MoinMoin:ThomasWaldmann
     @license: GNU GPL, see COPYING for details.
 """
 
