c08f95d354e73e19337428468f40ae5497fb369b
Thomas Waldmann <tw AT waldmann-edv DOT de@localhost>
2016-09-06 02:09:19 +0200

    from email.lowercase import ...

--- MoinMoin/mail/mailimport.py
+++ MoinMoin/mail/mailimport.py
@@ -11,8 +11,8 @@
 
 import sys, re, time
 import email
-from email.Utils import getaddresses, parsedate_tz, mktime_tz
-from email.Header import decode_header
+from email.utils import getaddresses, parsedate_tz, mktime_tz
+from email.header import decode_header
 
 from MoinMoin import wikiutil, user
 from MoinMoin.action.AttachFile import add_attachment, AttachmentAlreadyExists
