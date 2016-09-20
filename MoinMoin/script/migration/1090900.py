c1864ac162795e8fe54d76f4d5fba094a95f3741
TW <tw@waldmann-edv.de>
2016-09-20 20:49:51 +0200

    bump version to 1.9.9rc1, add dummy mig script

--- MoinMoin/script/migration/1090900.py
+++ MoinMoin/script/migration/1090900.py
@@ -1,12 +1,12 @@
 # -*- coding: iso-8859-1 -*-
 """
-    MoinMoin - migration from base rev 1090800
+    MoinMoin - dummy migration terminator script
 
-    Nothing to do, we just return the new data dir revision.
+    This must be the last migration script.
 
     @copyright: 2016 by Thomas Waldmann
     @license: GNU GPL, see COPYING for details.
 """
 
 def execute(script, data_dir, rev):
-    return 1090900
+    return None
