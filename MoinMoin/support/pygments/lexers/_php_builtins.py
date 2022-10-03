0c7807e7ee1e7e170ceee541d43a3a5a3a3311a9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-03 23:18:57 +0000

    Adding numeric_owner as keyword arguement

--- MoinMoin/support/pygments/lexers/_php_builtins.py
+++ MoinMoin/support/pygments/lexers/_php_builtins.py
@@ -4744,7 +4744,7 @@ if __name__ == '__main__':  # pragma: no cover
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar)
